8a25f2e6724d5320bf98948dc57afda653aaf331
Adam Jedlička <mail@adamjedlicka.cz>
2022-08-01 12:26:17 +0200

    Add GitHub workflows (#1)
    * Add GitHub workflow for Playwright tests
    
    * Add workflow for unit tests
    
    * Run bootstrap before unit tests
    
    * Add lint & tsc

--- modules/base-storefront/validators/same.ts
+++ modules/base-storefront/validators/same.ts
@@ -3,5 +3,5 @@ export default function same(this: any, value: any, field: any) {
 
   const label = this.$Form.inputs[field].label ?? field
 
-  return value === data[field] || 'Must be same as field' //this.$t('Must be same as "{0}" field', [label])
+  return value === data[field] || this.$t('Must be same as "{0}" field', [label])
 }
