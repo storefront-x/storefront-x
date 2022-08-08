c438c7729706d3daac3c9672256c75853c86f5ab
Jakub Rosina <32110080+kubo12312@users.noreply.github.com>
2022-08-08 17:19:57 +0200

    Remove tsc

---
+++ modules/catalog-magento/composables/useProduct.ts
@@ -0,0 +1,43 @@
+import useToProduct from '#ioc/mappers/useToProduct'
+import { computed, reactive, Ref } from 'vue'
+
+export default (product: Ref<ReturnType<ReturnType<typeof useToProduct>>>) => {
+  const id = computed(() => product.value.id)
+
+  const sku = computed(() => product.value.sku)
+
+  const name = computed(() => product.value.name)
+
+  const urlKey = computed(() => product.value.urlKey)
+
+  const urlPath = computed(() => product.value.urlPath)
+
+  const description = computed(() => product.value.descriptionHtml)
+
+  const shortDescriptionHtml = computed(() => product.value.shortDescriptionHtml)
+
+  const thumbnailUrl = computed(() => product.value.thumbnailUrl)
+
+  const regularPrice = computed(() => product.value.regularPrice)
+
+  const finalPrice = computed(() => product.value.finalPrice)
+
+  const available = computed(() => product.value.stockStatus)
+
+  const isOnSale = computed(() => finalPrice.value < regularPrice.value)
+
+  return reactive({
+    id,
+    sku,
+    name,
+    urlKey,
+    urlPath,
+    description,
+    shortDescriptionHtml,
+    thumbnailUrl,
+    regularPrice,
+    finalPrice,
+    available,
+    isOnSale,
+  })
+}
