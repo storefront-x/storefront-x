71bef16427e4595b7b03b763b4db58b5ddb540dd
Adam Jedlička <mail@adamjedlicka.cz>
2022-08-29 13:35:05 +0200

    Update create-script

---
+++ modules/create-storefront-x/template/shopware/modules/demo/pages/sign-up.vue
@@ -0,0 +1,16 @@
+<template>
+  <Signup :country-data="countryData" :salutations-data="salutationsData" />
+</template>
+
+<script setup lang="ts">
+import Signup from '#ioc/templates/SignUp'
+
+import useAsyncData from '#ioc/composables/useAsyncData'
+import useGetCountries from '#ioc/services/useGetCountries'
+import useGetSalutations from '#ioc/services/useGetSalutations'
+
+const getCountries = useGetCountries()
+const getSalutations = useGetSalutations()
+const { data: countryData } = useAsyncData('countries', () => getCountries())
+const { data: salutationsData } = useAsyncData('salutations', () => getSalutations())
+</script>
---
+++ storefront-x-app/modules/demo/paymentMethods/checkmo/payment.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>checkmo</div>
+</template>
