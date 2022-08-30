8e1706d35ae860e402bbc1bd243d4d7de69a14bf
Jakub Rosina <32110080+kubo12312@users.noreply.github.com>
2022-08-30 15:46:23 +0200

    Add multi-store and multi-currency for Magento (#32)

---
+++ modules/vue-i18n/components/SfxStoreSwitcher.vue
@@ -0,0 +1,40 @@
+<template>
+  <div v-bind="$attrs">
+    <slot v-bind="{ getUrlFor, currentStore, stores }" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import VUE_I18N_DIFFERENT_DOMAINS from '#ioc/config/VUE_I18N_DIFFERENT_DOMAINS'
+import { computed } from 'vue'
+import useRoute from '#ioc/composables/useRoute'
+import useLocalePath from '#ioc/composables/useLocalePath'
+import useSwitchLocalePath from '#ioc/composables/useSwitchLocalePath'
+import useCurrentStoreProperties from '#ioc/composables/useCurrentStoreProperties'
+
+const route = useRoute()
+const localePath = useLocalePath()
+const switchLocalePath = useSwitchLocalePath()
+const currentStoreProperties = useCurrentStoreProperties()
+
+const stores = computed(() => currentStoreProperties.stores)
+
+const currentStore = computed(() => currentStoreProperties.currentStore)
+
+const getUrlFor = (store: any) => {
+  if (_isCurrent(store)) return route.fullPath
+  if (_isCatchAllRoute()) {
+    return VUE_I18N_DIFFERENT_DOMAINS ? `//${store.domain}/` : localePath('/', store.name)
+  }
+  return switchLocalePath(store.name)
+}
+
+const _isCurrent = (store: any) => {
+  return currentStore?.value?.name === store.name
+}
+
+const _isCatchAllRoute = () => {
+  if (!route.name) return false
+  return route.name.toString().startsWith('all___')
+}
+</script>
