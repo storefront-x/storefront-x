47d975808dcef593fc8792753f256ff6eef96b73
Michal Soska <extSoska@dr-max.global>
2022-09-01 18:05:49 +0200

    PX-1311: recursive category mapper

--- modules/catalog-magento/mappers/useToCategory.ts
+++ modules/catalog-magento/mappers/useToCategory.ts
@@ -1,18 +1,19 @@
 import useCatalogMagentoStore from '#ioc/stores/useCatalogMagentoStore'
-import useToSubcategory from '#ioc/mappers/useToSubcategory'
+import MAGENTO_URL from '#ioc/config/MAGENTO_URL'
 
 export default () => {
   const catalogMagentoStore = useCatalogMagentoStore()
-  const toSubcategory = useToSubcategory()
 
-  return (data: any) => ({
+  const toCategory = (data: any) => ({
     id: data.id ?? 0,
     urlPath: '/' + data.url_path + catalogMagentoStore.categoryUrlSuffix,
     name: data.name ?? '',
     description: data.description ?? '',
-    thumbnailUrl: data.thumbnail ? '/media/catalog/category/' + data.thumbnail : data.thumbnail,
+    thumbnailUrl: data.thumbnail ? MAGENTO_URL + '/media/catalog/category/' + data.thumbnail : data.thumbnail,
     breadcrumbs: [],
     productsTotalCount: data.products?.total_count ?? 0,
-    children: data.children.map(toSubcategory) ?? [],
+    children: data.children?.map(toCategory) ?? [],
   })
+
+  return toCategory
 }
