c015cc5ed8964819dd0991335b00c265600e9c52
Jaroslav <jaroslavskudrna@gmail.com>
2022-09-01 22:34:44 +0200

    adding next to else

--- modules/demo-shopware/pages/account/addresses.beforeEnter.ts
+++ modules/demo-shopware/pages/account/addresses.beforeEnter.ts
@@ -1,12 +1,11 @@
 import useCustomer from '#ioc/composables/useCustomer'
-import useLocalePath from '#ioc/composables/useLocalePath'
 
 export default (to: any, from: any, next: any) => {
   const customer = useCustomer()
-  const localePath = useLocalePath()
 
-  if (!customer.isLoggedIn) {
-    next(localePath('/sign-in'))
+  if (!customer?.isLoggedIn) {
+    next('/sign-in')
+  } else {
+    next()
   }
-  next()
 }
