bbb9a4a326322b471c776a2572e17bbb692930f1
Jakub Rosina <32110080+kubo12312@users.noreply.github.com>
2022-09-02 16:54:47 +0200

    Added carousel magento PageBuilder blocks (#37)

--- modules/theme-tailwind-magento/magento/cms/blocks/pb-products.vue
+++ modules/theme-tailwind-magento/magento/cms/blocks/pb-products.vue
@@ -1,3 +1,31 @@
 <template>
-  <div>TODO</div>
+  <ProductCarousel v-if="products.length" :products="products" />
 </template>
+
+<script>
+import { defineComponent } from 'vue'
+import useGetProductsByIds from '#ioc/services/useGetProductsByIds'
+import IsPbBlock from '#ioc/mixins/IsPbBlock'
+import IsPbProducts from '#ioc/mixins/IsPbProducts'
+import ProductCarousel from '#ioc/organisms/ProductCarousel'
+import usePbProducts from '#ioc/composables/usePbProducts'
+import useAsyncData from '#ioc/composables/useAsyncData'
+
+export default defineComponent({
+  components: {
+    ProductCarousel,
+  },
+
+  mixins: [IsPbBlock, IsPbProducts],
+
+  async setup(props) {
+    const pbProducts = usePbProducts(props.el)
+    const getProductsByIds = useGetProductsByIds()
+    const { data } = await useAsyncData('carouselProduct', () => getProductsByIds(pbProducts.skus))
+
+    return {
+      products: data.value.products,
+    }
+  },
+})
+</script>
