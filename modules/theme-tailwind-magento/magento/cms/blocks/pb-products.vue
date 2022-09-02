dbada8d9078195520a78ccec5da3999f3d9fd0d8
Jakub Rosina <32110080+kubo12312@users.noreply.github.com>
2022-09-02 11:16:47 +0200

    Fetching data in setup function

--- modules/theme-tailwind-magento/magento/cms/blocks/pb-products.vue
+++ modules/theme-tailwind-magento/magento/cms/blocks/pb-products.vue
@@ -8,6 +8,8 @@ import useGetProductsByIds from '#ioc/services/useGetProductsByIds'
 import IsPbBlock from '#ioc/mixins/IsPbBlock'
 import IsPbProducts from '#ioc/mixins/IsPbProducts'
 import ProductCarousel from '#ioc/organisms/ProductCarousel'
+import usePbProducts from '#ioc/composables/usePbProducts'
+import useAsyncData from '#ioc/composables/useAsyncData'
 
 export default defineComponent({
   components: {
@@ -16,14 +18,14 @@ export default defineComponent({
 
   mixins: [IsPbBlock, IsPbProducts],
 
-  data: () => ({
-    products: [],
-  }),
-
-  async mounted() {
+  async setup(props) {
+    const pbProducts = usePbProducts(props.el)
     const getProductsByIds = useGetProductsByIds()
-    const { products } = await getProductsByIds(this.skus)
-    this.products = products
+    const { data } = await useAsyncData('carouselProduct', () => getProductsByIds(pbProducts.skus))
+
+    return {
+      products: data.value.products,
+    }
   },
 })
 </script>
