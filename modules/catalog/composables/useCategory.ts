2d40263db4dd1a0bdd5bc4f8607f1b51cf5fc865
Jakub Rosina <32110080+kubo12312@users.noreply.github.com>
2022-09-05 18:02:56 +0200

    Add nested menu for Shopware (#64)

--- modules/catalog/composables/useCategory.ts
+++ modules/catalog/composables/useCategory.ts
@@ -1,16 +1,24 @@
 import ToCategory from '#ioc/mappers/ToCategory'
 import { computed, reactive, Ref } from 'vue'
 
-export default (product: Ref<ReturnType<typeof ToCategory>>) => {
-  const id = computed(() => product.value.id)
+export default (category: Ref<ReturnType<typeof ToCategory>>) => {
+  const id = computed(() => category.value.id)
 
-  const name = computed(() => product.value.name)
+  const name = computed(() => category.value.name)
 
-  const urlPath = computed(() => product.value.urlPath)
+  const urlPath = computed(() => category.value.urlPath)
+
+  const children = computed(() => {
+    return category.value.children
+  })
+
+  const breadcrumbs = computed(() => category.value.breadcrumbs)
 
   return reactive({
     id,
     name,
     urlPath,
+    children,
+    breadcrumbs,
   })
 }
