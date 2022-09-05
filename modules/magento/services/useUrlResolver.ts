f784f7a9af44b192ac5912e0b2a21d57eae53d3c
Jakub Rosina <32110080+kubo12312@users.noreply.github.com>
2022-09-05 11:46:59 +0200

    Fix subcategory navigation (#59)

--- modules/magento/services/useUrlResolver.ts
+++ modules/magento/services/useUrlResolver.ts
@@ -1,6 +1,7 @@
 import useAsyncData from '#ioc/composables/useAsyncData'
 import useRoute from '#ioc/composables/useRoute'
 import useUrlResolverRepository from '#ioc/repositories/useUrlResolverRepository'
+import ensureArray from '#ioc/utils/array/ensureArray'
 import dynamicPages from '~/.sfx/magento/dynamicPages'
 
 export default () => {
@@ -8,8 +9,10 @@ export default () => {
   const urlResolverRepository = useUrlResolverRepository()
 
   return async (): Promise<{ id: string; component: any; relativeUrl: string }> => {
-    const path = route.params?.pathMatch?.toString().replace(',', '/') || '/'
-    const { data } = await useAsyncData('urlResolver', () => urlResolverRepository(path))
+    const path = ensureArray(route.params?.pathMatch)
+    const routePath = path.length ? path.join('/') : '/'
+
+    const { data } = await useAsyncData('urlResolver', () => urlResolverRepository(routePath))
 
     return {
       id: data.value.id,
