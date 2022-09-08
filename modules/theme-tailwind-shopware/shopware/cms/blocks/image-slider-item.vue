4852d65224698695c4c8b875c89d576381fc74ed
Adam Jedlička <mail@adamjedlicka.cz>
2022-09-08 11:30:59 +0200

    Use RouterLink in image-slider-item

--- modules/theme-tailwind-shopware/shopware/cms/blocks/image-slider-item.vue
+++ modules/theme-tailwind-shopware/shopware/cms/blocks/image-slider-item.vue
@@ -1,11 +1,12 @@
 <template>
-  <a :href="sliderItem.url" :target="sliderItem.newTab" class="inline-block relative w-full h-[300px]">
+  <RouterLink :to="localePath(sliderItem.url)" class="inline-block relative w-full h-[300px]">
     <SfxImage fit="cover" :src="sliderItem.media.url" class="w-full h-full mx-auto object-cover" />
-  </a>
+  </RouterLink>
 </template>
 
 <script setup lang="ts">
 import SfxImage from '#ioc/components/SfxImage'
+import useLocalePath from '#ioc/composables/useLocalePath'
 
 defineProps({
   sliderItem: {
@@ -17,4 +18,6 @@ defineProps({
     default: 300,
   },
 })
+
+const localePath = useLocalePath()
 </script>
