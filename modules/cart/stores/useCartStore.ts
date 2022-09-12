496cc2bb2aed31de19ed008aa33bb60ce61c3450
Jakub Rosina <32110080+kubo12312@users.noreply.github.com>
2022-09-12 14:40:33 +0200

    Remove cart id from store

--- modules/cart/stores/useCartStore.ts
+++ modules/cart/stores/useCartStore.ts
@@ -4,6 +4,5 @@ import { defineStore } from 'pinia'
 export default defineStore('cart', {
   state: () => ({
     cart: undefined as ReturnType<typeof ToCart> | undefined | null,
-    cartId: '',
   }),
 })
