9f19fb3f1d08ca74678492005dcb3a6170671d3b
Jakub Rosina <32110080+kubo12312@users.noreply.github.com>
2022-09-12 15:06:29 +0200

    Fix error with invalid cart ID (#62)

--- modules/cart-magento/services/useGetCart.ts
+++ modules/cart-magento/services/useGetCart.ts
@@ -1,20 +1,43 @@
 import ToCart from '#ioc/mappers/ToCart'
 import useGetCartByIdRepository from '#ioc/repositories/useGetCartByIdRepository'
 import useGetOrCreateCartId from '#ioc/services/useGetOrCreateCartId'
+import useCookies from '#ioc/composables/useCookies'
+import MAGENTO_CART_COOKIE_NAME from '#ioc/config/MAGENTO_CART_COOKIE_NAME'
+import useCartMagentoStore from '#ioc/stores/useCartMagentoStore'
 
 export default () => {
   const getOrCreateCartId = useGetOrCreateCartId()
   const getCartByIdRepository = useGetCartByIdRepository()
+  const cookies = useCookies()
+  const cartMagentoStore = useCartMagentoStore()
 
   return async (): Promise<{
     cart: ReturnType<typeof ToCart>
   }> => {
     const { id } = await getOrCreateCartId()
 
-    const { cart } = await getCartByIdRepository(id)
+    try {
+      const { cart } = await getCartByIdRepository(id)
 
-    return {
-      cart,
+      return {
+        cart,
+      }
+    } catch (e: any) {
+      if (e.data.category === 'graphql-no-such-entity') {
+        cookies.remove(MAGENTO_CART_COOKIE_NAME)
+        cartMagentoStore.$patch({ cartId: '' })
+
+        const { id } = await getOrCreateCartId()
+
+        const { cart } = await getCartByIdRepository(id)
+
+        return {
+          cart,
+        }
+      } else {
+        console.error(e)
+        throw e
+      }
     }
   }
 }
--- modules/cart-magento/services/useGetOrCreateCartId.ts
+++ modules/cart-magento/services/useGetOrCreateCartId.ts
@@ -1,20 +1,24 @@
 import useCookies from '#ioc/composables/useCookies'
 import useCreateEmptyCartRepository from '#ioc/repositories/useCreateEmptyCartRepository'
 import MAGENTO_CART_COOKIE_NAME from '#ioc/config/MAGENTO_CART_COOKIE_NAME'
+import useCartMagentoStore from '#ioc/stores/useCartMagentoStore'
 
 export default () => {
   const cookies = useCookies()
   const createEmptyCartRepository = useCreateEmptyCartRepository()
+  const cartMagentoStore = useCartMagentoStore()
 
   return async (): Promise<{
     id: string
   }> => {
-    const id = cookies.get(MAGENTO_CART_COOKIE_NAME)
+    const id = cartMagentoStore.cartId
     if (id) return { id }
 
     {
       const { id } = await createEmptyCartRepository()
       cookies.set(MAGENTO_CART_COOKIE_NAME, id)
+
+      cartMagentoStore.$patch({ cartId: id })
 
       return { id }
     }
