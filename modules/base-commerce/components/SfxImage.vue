b66a63df09b4dc154cf24dffcc041a8ba99db66d
Adam Jedlička <mail@adamjedlicka.cz>
2022-09-12 15:11:43 +0200

    Fix Lightbox (#78)

--- modules/base-commerce/components/SfxImage.vue
+++ modules/base-commerce/components/SfxImage.vue
@@ -15,102 +15,93 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-export default defineComponent({
-  inheritAttrs: false,
-})
-</script>
-
-<script setup lang="ts">
 import ensureArray from '#ioc/utils/array/ensureArray'
 import resizeImage from '#ioc/utils/url/resizeImage'
-import { computed, PropType } from 'vue'
+import { defineComponent, PropType } from 'vue'
 
-const props = defineProps({
-  lazy: {
-    type: Boolean,
-    default: false,
+export default defineComponent({
+  inheritAttrs: false,
+
+  props: {
+    lazy: {
+      type: Boolean,
+      default: false,
+    },
+    src: {
+      type: String,
+      default: null,
+    },
+    srcSfx: {
+      type: String,
+      default: null,
+    },
+    width: {
+      type: Number,
+      default: null,
+    },
+    height: {
+      type: Number,
+      default: null,
+    },
+    fit: {
+      type: String as PropType<'cover' | 'contain' | 'fill' | 'inside' | 'outside'>,
+      default: null,
+      validator: (value: string) => ['cover', 'contain', 'fill', 'inside', 'outside'].includes(value),
+    },
+    format: {
+      type: [Array, String],
+      default: () => ['webp', 'jpeg'],
+    },
+    multiples: {
+      type: [Array, Number],
+      default: () => [],
+    },
+    alt: {
+      type: String,
+      default: undefined,
+    },
+    classImg: {
+      type: [String, Array, Object],
+      default: undefined,
+    },
+    preload: {
+      type: Boolean,
+      default: false,
+    },
   },
-  src: {
-    type: String,
-    default: null,
+
+  computed: {
+    source() {
+      return this.resizeImage()
+    },
+
+    sources() {
+      return ensureArray(this.format).map((format: string) => {
+        const multiples = ensureArray(this.multiples).map(
+          (multiple: number) => this.resizeImage({ format, multiple }) + ` ${multiple}x`,
+        )
+
+        return [`image/${format}`, [this.resizeImage({ format }), ...multiples].join(', ')]
+      })
+    },
+
+    loading() {
+      return this.lazy ? 'lazy' : 'eager'
+    },
   },
-  srcM2: {
-    type: String,
-    default: null,
-  },
-  srcSfx: {
-    type: String,
-    default: null,
-  },
-  srcErgonode: {
-    type: String,
-    default: null,
-  },
-  width: {
-    type: Number,
-    default: null,
-  },
-  height: {
-    type: Number,
-    default: null,
-  },
-  fit: {
-    type: String as PropType<'cover' | 'contain' | 'fill' | 'inside' | 'outside'>,
-    default: null,
-    validator: (value: string) => ['cover', 'contain', 'fill', 'inside', 'outside'].includes(value),
-  },
-  format: {
-    type: [Array, String],
-    default: () => ['webp', 'jpeg'],
-  },
-  multiples: {
-    type: [Array, Number],
-    default: () => [],
-  },
-  alt: {
-    type: String,
-    default: undefined,
-  },
-  classImg: {
-    type: [String, Array, Object],
-    default: undefined,
-  },
-  preload: {
-    type: Boolean,
-    default: false,
+
+  methods: {
+    resizeImage({ format = 'jpeg', multiple = 1, ...rest } = {}) {
+      return resizeImage({
+        path: this.src,
+        sfx: this.srcSfx,
+        w: this.width && this.width * multiple,
+        h: this.height && this.height * multiple,
+        fit: this.fit,
+        format,
+        ...rest,
+      })
+    },
   },
 })
-
-const source = computed(() => {
-  return resizeImageFunction()
-})
-
-const sources = computed(() => {
-  return ensureArray(props.format).map((format: string) => {
-    const multiples = ensureArray(props.multiples).map(
-      (multiple: number) => resizeImageFunction({ format, multiple }) + ` ${multiple}x`,
-    )
-
-    return [`image/${format}`, [resizeImageFunction({ format }), ...multiples].join(', ')]
-  })
-})
-
-const loading = computed(() => {
-  return props.lazy ? 'lazy' : 'eager'
-})
-
-const resizeImageFunction = ({ format = 'jpeg', multiple = 1, ...rest } = {}) => {
-  return resizeImage({
-    path: props.src,
-    m2: props.srcM2,
-    sfx: props.srcSfx,
-    ergonode: props.srcErgonode,
-    w: props.width && props.width * multiple,
-    h: props.height && props.height * multiple,
-    fit: props.fit,
-    format,
-    ...rest,
-  })
-}
 </script>
