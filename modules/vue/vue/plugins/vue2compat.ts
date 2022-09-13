a042c8dad547481047cf1521f96b645fa3bdaa10
Adam Jedlička <mail@adamjedlicka.cz>
2022-09-13 10:36:07 +0200

    Fix crash caused by vue2compat

--- modules/vue/vue/plugins/vue2compat.ts
+++ modules/vue/vue/plugins/vue2compat.ts
@@ -3,11 +3,11 @@ import type { App } from 'vue'
 export default (app: App) => {
   app.mixin({
     mounted() {
-      this.$el.__vue__ = this
+      if (this.$el) this.$el.__vue__ = this
     },
 
     updated() {
-      this.$el.__vue__ = this
+      if (this.$el) this.$el.__vue__ = this
     },
   })
 }
