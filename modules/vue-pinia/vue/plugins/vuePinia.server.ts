941e37029eee71f235523abe59b2f0e86447bd4c
Adam Jedlička <mail@adamjedlicka.cz>
2022-10-14 16:52:11 +0200

    Upgrade deps (#119)

--- modules/vue-pinia/vue/plugins/vuePinia.server.ts
+++ modules/vue-pinia/vue/plugins/vuePinia.server.ts
@@ -1,8 +1,8 @@
 import type { App } from 'vue'
-import { devalue } from 'devalue'
+import { uneval } from 'devalue'
 
 export const after = async (app: App, ctx?: any) => {
-  const rendered = devalue(ctx.$pinia.state.value ?? {})
+  const rendered = uneval(ctx.$pinia.state.value ?? {})
 
   ctx.out.pinia = (html: string) => html.replace('</body>', `<script>window.$pinia=${rendered}</script></body>`)
 }
