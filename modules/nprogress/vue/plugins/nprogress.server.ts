c005d39dcdaa7f9e471737656a396c9f19cf761e
Jan Vaněk <55270236+JanVanekRelitas@users.noreply.github.com>
2022-10-19 11:22:03 +0200

    Add loader when navigating between pages (#123)

---
+++ modules/nprogress/vue/plugins/nprogress.server.ts
@@ -0,0 +1,21 @@
+import type { App } from 'vue'
+import NPROGRESS_BAR_COLOR from '#ioc/config/NPROGRESS_BAR_COLOR'
+import NPROGRESS_BAR_HEIGHT from '#ioc/config/NPROGRESS_BAR_HEIGHT'
+
+const style = `<style>
+  #nprogress .bar {
+    background: ${NPROGRESS_BAR_COLOR};
+    position: fixed;
+    z-index: 1031;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: ${NPROGRESS_BAR_HEIGHT}px;
+  }
+  </style>`
+
+const inlineStyle = style.replace(/\s\s+/gs, ' ')
+
+export const after = async (app: App, ctx?: any) => {
+  ctx.out.cookieScript = (html: string) => html.replace('<head>', `<head>${inlineStyle}`)
+}
