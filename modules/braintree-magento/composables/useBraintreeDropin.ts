96d9fd22f237db6ca9cefd3b4537d2660f143b5d
Adam Jedlička <mail@adamjedlicka.cz>
2022-10-21 17:04:27 +0200

    Dont use default export of braintree-web-drop-in

--- modules/braintree-magento/composables/useBraintreeDropin.ts
+++ modules/braintree-magento/composables/useBraintreeDropin.ts
@@ -1,18 +1,18 @@
-import dropin from 'braintree-web-drop-in'
+import { create, Dropin } from 'braintree-web-drop-in'
 import useMagento from '#ioc/composables/useMagento'
 import CreateBraintreeClientToken from '#ioc/graphql/mutations/CreateBraintreeClientToken'
 
 export default () => {
   const magento = useMagento()
 
-  let instance: dropin.Dropin
+  let instance: Dropin
 
   const initialize = async (container: string) => {
     const { data } = await magento.graphql(CreateBraintreeClientToken())
 
     const authorization = data.createBraintreeClientToken
 
-    instance = await dropin.create({
+    instance = await create({
       authorization,
       container,
     })
