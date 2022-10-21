05fba010ddfc4049c14ec0fffb2aca666043d5c3
Jaroslav <jaroslavskudrna@gmail.com>
2022-10-21 10:52:51 +0200

    reset merge wishlist

--- modules/wishlist-magento/repositories/useMergeWishlistRepository.ts
+++ modules/wishlist-magento/repositories/useMergeWishlistRepository.ts
@@ -1,25 +1,19 @@
 import useMagento from '#ioc/composables/useMagento'
 import ToProduct from '#ioc/mappers/ToProduct'
 import AddProductsToWishlist from '#ioc/graphql/mutations/AddProductsToWishlist'
-import ToWishlistUserError from '#ioc/mappers/ToWishlistUserError'
 
 export default () => {
   const magento = useMagento()
 
   return async (products: ReturnType<typeof ToProduct>[], id: any) => {
-    try {
-      const {
-        data: { addProductsToWishlist },
-      } = await magento.graphql(AddProductsToWishlist().with({ id, items: products }))
+    const {
+      data: { addProductsToWishlist },
+    } = await magento.graphql(AddProductsToWishlist().with({ id, items: products }))
 
-      return {
-        userErrors: addProductsToWishlist?.user_errors?.map(ToWishlistUserError),
-      }
-    } catch (e) {
-      console.warn(e)
-      return {
-        wishlist: [],
-      }
+    if (!addProductsToWishlist) throw new Error()
+
+    return {
+      wishlist: addProductsToWishlist.wishlist,
     }
   }
 }
