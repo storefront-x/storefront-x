e806b8a6be6edc63d031049bfaa4189f4d56c6e0
JanVanekRelitas <janvanek@relitas.cz>
2022-10-25 11:41:51 +0200

    ready to test

--- modules/vue-i18n/vueRouter/plugins/i18nRoutes.ts
+++ modules/vue-i18n/vueRouter/plugins/i18nRoutes.ts
@@ -2,64 +2,40 @@ import VUE_I18N_LOCALES from '#ioc/config/VUE_I18N_LOCALES'
 import VUE_I18N_ROUTE_PATHS from '#ioc/config/VUE_I18N_ROUTE_PATHS'
 
 export default (routes: any) => {
-  const newRoutes = []
-
-  for (const route of routes) {
-    for (const locale of VUE_I18N_LOCALES) {
-      newRoutes.push(processRoute(route, locale))
-    }
-  }
-
-  return newRoutes
+  return routes.map((route: any) => processRoute(route))
 }
 
-const processRoute = (route: any, locale: any) => {
+const processRoute = (route: any) => {
   return {
     ...route,
-    name: getName(route, locale),
-    path: getPath(route, locale),
-    children: getChildren(route, locale),
+    alias: getAlias(route),
+    children: getChildren(route),
   }
 }
 
-const getName = (route: any, locale: any) => {
-  if (route.name === undefined) {
-    return undefined
-  }
-
-  const isIndexPage = route.name === 'index'
-  const isIndexInFolder = route.name.endsWith('index')
-
-  if (isIndexPage) {
-    return `${route.name.replace('/', '')}__${locale.name}`
-  }
-
-  if (isIndexInFolder) {
-    return `${route.name.replace('index', '').replace('/', '')}__${locale.name}`
-  }
-
-  return `${route.name}__${locale.name}`
-}
-
-const getPath = (route: any, locale: any) => {
-  const path = (VUE_I18N_ROUTE_PATHS as any)[route.path]?.[locale.name] ?? route.path
-
-  if (locale.prefix === '/') {
-    return path
-  } else {
-    return locale.prefix + path
+const getAlias = (route: any) => {
+  const aliases = []
+  for (const locale of VUE_I18N_LOCALES) {
+    const aliasLocale = (VUE_I18N_ROUTE_PATHS as any)[route.path]?.[locale.name] ?? null
+    const aliasPrefix = locale.prefix === '/' ? null : locale.prefix
+    if (aliasLocale && aliasPrefix) {
+      aliases.push(`${aliasPrefix}${aliasLocale}`)
+    } else if (aliasLocale) {
+      aliases.push(`${aliasLocale}`)
+    } else if (aliasPrefix) {
+      aliases.push(`${aliasPrefix}${route.path}`)
+    }
+    return aliases
   }
 }
-
-const getChildren = (route: any, locale: any): any => {
+const getChildren = (route: any): any => {
   const newChildren = []
 
   for (const child of route.children ?? []) {
     newChildren.push({
       ...child,
-      name: getName(child, locale),
-      path: getPath(child, locale),
-      children: getChildren(child, locale),
+      alias: getAlias(route),
+      children: getChildren(child),
     })
   }
 
