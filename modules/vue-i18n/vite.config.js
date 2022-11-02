bffc75df1f2fbcfb4e7a98524b25cfa3ffb19e5e
Jaroslav <jaroslavskudrna@gmail.com>
2022-11-02 18:21:20 +0100

    translations i18n  with parms not working in production

--- modules/vue-i18n/vite.config.js
+++ modules/vue-i18n/vite.config.js
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 import vueI18n from '@intlify/vite-plugin-vue-i18n'
 
 export default defineConfig({
-  plugins: [vueI18n()],
+  plugins: [vueI18n({ runtimeOnly: false })],
 })
