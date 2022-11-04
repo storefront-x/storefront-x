6dd088c9e5a27f931ecbd6e04b5be8ba042f7256
Jaroslav <jaroslavskudrna@gmail.com>
2022-11-04 16:14:11 +0100

    CR changes

--- modules/product-reviews-magento/graphql/queries/CategoryDetail.ext.ts
+++ modules/product-reviews-magento/graphql/queries/CategoryDetail.ext.ts
@@ -1,6 +1,4 @@
 import field from '#ioc/graphql/field'
-import Review from '#ioc/graphql/fragments/Review'
-import CATALOG_REVIEWS_PER_PAGE from '#ioc/config/CATALOG_REVIEWS_PER_PAGE'
 import addFields from '#ioc/utils/graphql/addFields'
 
 export default (self: any) => {
@@ -10,15 +8,6 @@ export default (self: any) => {
     addFields(query, 'products.items', {
       rating_summary: field(),
       review_count: field(),
-      reviews: field()
-        .args({
-          pageSize: CATALOG_REVIEWS_PER_PAGE,
-        })
-        .fields({
-          items: field({
-            ...Review(),
-          }),
-        }),
     })
 
     return query
