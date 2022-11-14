048f0b472a44eb58f76dad32484ffdd7cbe2b0f9
Jaroslav <jaroslavskudrna@gmail.com>
2022-11-14 13:08:07 +0100

    maybe better

--- modules/theme-tailwind/organisms/CrossSellModal.vue
+++ modules/theme-tailwind/organisms/CrossSellModal.vue
@@ -1,71 +1,74 @@
 <template>
-  <Modal v-show="data?.products.length" @close="emit('close')">
-    <div>
-      <div class="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
-        <svg
-          class="h-6 w-6 text-green-600"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          aria-hidden="true"
-        >
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
-        </svg>
+  <Modal @close="emit('close')">
+    <div v-show="data?.products.length">
+      <div>
+        <div class="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
+          <svg
+            class="h-6 w-6 text-green-600"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            aria-hidden="true"
+          >
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+          </svg>
+        </div>
+        <div class="mt-3 text-center sm:mt-5">
+          <h3 class="text-lg leading-6 font-medium text-gray-900">
+            {{ t('Added to cart') }}
+          </h3>
+        </div>
       </div>
-      <div class="mt-3 text-center sm:mt-5">
-        <h3 class="text-lg leading-6 font-medium text-gray-900">
-          {{ t('Added to cart') }}
-        </h3>
+      <div class="mt-5 sm:mt-6 sm:grid sm:grid-cols-2 sm:gap-3 sm:grid-flow-row-dense">
+        <Button class="w-full" data-cy="continue-shopping" @click="emit('close')">
+          {{ t('Continue shopping') }}
+        </Button>
+        <Button :to="localePath('checkout')" color="primary" class="mt-3 sm:mt-0" data-cy="continue-to-checkout">
+          {{ t('Continue to checkout') }}
+        </Button>
       </div>
-    </div>
-    <div class="mt-5 sm:mt-6 sm:grid sm:grid-cols-2 sm:gap-3 sm:grid-flow-row-dense">
-      <Button class="w-full" data-cy="continue-shopping" @click="emit('close')">
-        {{ t('Continue shopping') }}
-      </Button>
-      <Button :to="localePath('checkout')" color="primary" class="mt-3 sm:mt-0" data-cy="continue-to-checkout">
-        {{ t('Continue to checkout') }}
-      </Button>
-    </div>
-    <div class="mt-2">
-      <h5 class="w-100 font-medium text-lg text-gray-900 text-center my-4">
-        {{ t('Other people also like to buy') }}:
-      </h5>
+      <div class="mt-2">
+        <h5 class="w-100 font-medium text-lg text-gray-900 text-center my-4">
+          {{ t('Other people also like to buy') }}:
+        </h5>
 
-      <ul class="divide-y divide-gray-200 text-sm font-medium text-gray-900">
-        <ProductProvider v-for="crossSell in data.products" :key="crossSell.sku" :product="crossSell">
-          <CrossSellProduct />
-        </ProductProvider>
-      </ul>
-    </div>
-  </Modal>
-  <Modal v-show="!data?.products.length" @close="emit('close')">
-    <div>
-      <div class="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
-        <svg
-          class="h-6 w-6 text-green-600"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          aria-hidden="true"
-        >
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
-        </svg>
-      </div>
-      <div class="mt-3 text-center sm:mt-5">
-        <h3 class="text-lg leading-6 font-medium text-gray-900">
-          {{ t('Added to cart') }}
-        </h3>
+        <ul class="divide-y divide-gray-200 text-sm font-medium text-gray-900">
+          <ProductProvider v-for="crossSell in data.products" :key="crossSell.sku" :product="crossSell">
+            <CrossSellProduct />
+          </ProductProvider>
+        </ul>
       </div>
     </div>
-    <div class="mt-5 sm:mt-6 sm:grid sm:grid-cols-2 sm:gap-3 sm:grid-flow-row-dense">
-      <Button class="w-full" data-cy="continue-shopping" @click="emit('close')">
-        {{ t('Continue shopping') }}
-      </Button>
-      <Button :to="localePath('checkout')" color="primary" class="mt-3 sm:mt-0" data-cy="continue-to-checkout">
-        {{ t('Continue to checkout') }}
-      </Button>
+
+    <div v-show="!data?.products.length">
+      <div>
+        <div class="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
+          <svg
+            class="h-6 w-6 text-green-600"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            aria-hidden="true"
+          >
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+          </svg>
+        </div>
+        <div class="mt-3 text-center sm:mt-5">
+          <h3 class="text-lg leading-6 font-medium text-gray-900">
+            {{ t('Added to cart') }}
+          </h3>
+        </div>
+      </div>
+      <div class="mt-5 sm:mt-6 sm:grid sm:grid-cols-2 sm:gap-3 sm:grid-flow-row-dense">
+        <Button class="w-full" data-cy="continue-shopping" @click="emit('close')">
+          {{ t('Continue shopping') }}
+        </Button>
+        <Button :to="localePath('checkout')" color="primary" class="mt-3 sm:mt-0" data-cy="continue-to-checkout">
+          {{ t('Continue to checkout') }}
+        </Button>
+      </div>
     </div>
   </Modal>
 </template>
