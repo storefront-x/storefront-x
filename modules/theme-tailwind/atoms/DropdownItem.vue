edb7d5182fc2e93a038fe676f6637ad9fee3ebf2
Jaroslav <jaroslavskudrna@gmail.com>
2022-11-21 10:27:14 +0100

    css changes

--- modules/theme-tailwind/atoms/DropdownItem.vue
+++ modules/theme-tailwind/atoms/DropdownItem.vue
@@ -1,7 +1,7 @@
 <template>
   <Component
     :is="$attrs.to ? 'RouterLink' : 'a'"
-    class="font-medium text-gray-900 hover:bg-gray-100 block px-4 py-2"
+    class="font-medium text-gray-900 hover:bg-gray-100 block px-4 py-2 cursor-pointer"
     role="menuitem"
     tabindex="-1"
   >
