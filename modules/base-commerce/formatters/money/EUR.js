fed738c58535e1b307d24b50d257652eac6950cc
JaroslavSku <47352588+JaroslavSku@users.noreply.github.com>
2022-11-29 13:18:14 +0100

    Fix price formats (#188)

--- modules/base-commerce/formatters/money/EUR.js
+++ modules/base-commerce/formatters/money/EUR.js
@@ -1,1 +1,9 @@
-export default (value) => (value < 0 ? '-' : '') + '€' + value.toFixed(2)
+const intl = new Intl.NumberFormat('sfb', { style: 'currency', currency: 'EUR' })
+const intlNumber = new Intl.NumberFormat('sfb', {
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+})
+
+export default (value) => intl.format(value).replace(/,00/, '')
+
+export const noSign = (value) => intlNumber.format(value).replace(/,00/, '')
