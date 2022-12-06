6df72ecb44d8f5b4c9893beb977c406e9ec29185
Jakub Rosina <32110080+kubo12312@users.noreply.github.com>
2022-12-06 11:48:40 +0100

    Remove unused imports

--- modules/customer-magento/magento/errorHandlers/handleAuthorizationError.ts
+++ modules/customer-magento/magento/errorHandlers/handleAuthorizationError.ts
@@ -1,8 +1,6 @@
 import useCookies from '#ioc/composables/useCookies'
 import MAGENTO_CUSTOMER_COOKIE_NAME from '#ioc/config/MAGENTO_CUSTOMER_COOKIE_NAME'
 import CustomerNotAuthorized from '#ioc/errors/CustomerNotAuthorized'
-import useCartStore from '#ioc/stores/useCartStore'
-import useCheckoutStore from '#ioc/stores/useCheckoutStore'
 import useCustomerStore from '#ioc/stores/useCustomerStore'
 import isAuthorizationError from '#ioc/utils/graphql/isAuthorizationError'
 
