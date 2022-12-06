a7888a72ad3170edae34c507069159669757e4e9
Adam Jedlička <mail@adamjedlicka.cz>
2022-12-06 16:59:45 +0100

    Fix Cypress pipeline (#205)

--- modules/cart-magento/services/useGetCustomerCartId.ts
+++ modules/cart-magento/services/useGetCustomerCartId.ts
@@ -1,14 +1,9 @@
 import useGetCustomerCartIdRepository from '#ioc/repositories/useGetCustomerCartIdRepository'
-import useCartMagentoStore from '#ioc/stores/useCartMagentoStore'
 
 export default () => {
   const getCustomerCartIdRepository = useGetCustomerCartIdRepository()
-  const cartMagentoStore = useCartMagentoStore()
 
   return async () => {
-    const cartId = cartMagentoStore.cartId
-    if (cartId) return { cartId }
-
     const { id } = await getCustomerCartIdRepository()
 
     return { id }
