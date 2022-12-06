4f01d165294aadf37188b7b8565f1e29ca178c9e
JanVanekRelitas <janvanek@relitas.cz>
2022-12-06 17:34:18 +0100

    added test

--- modules/vue-router-simple/tests/playwright/isExactActive.spec.js
+++ modules/vue-router-simple/tests/playwright/isExactActive.spec.js
@@ -94,3 +94,42 @@ test.skip('is same path with params', async ({ page }) => {
     },
   )
 })
+
+test.only('exactActiveClass and exactInactiveClass', async ({ page }) => {
+  await makeProject(
+    {
+      modules: [
+        '@storefront-x/base',
+        '@storefront-x/vue',
+        '@storefront-x/vue-router-simple',
+        [
+          'my-module',
+          {
+            pages: {
+              user: {
+                'test.vue': `
+                  <template>
+                    <RouterLink
+                      v-slot="{ isExactActive }" to="/user/test?working=true"
+                      exact-active-class="activeClass"
+                      exact-inactive-class="inactiveClass"
+                    >
+                      <h1>LINK</h1>
+                    </RouterLink>
+                  </template>`,
+              },
+            },
+          },
+        ],
+      ],
+    },
+    async ({ url }) => {
+      await page.goto(url + '/user/test?working=true', { waitUntil: 'networkidle' })
+      await expect(page.locator('a')).toHaveClass('activeClass')
+      await expect(page.locator('a')).not.toHaveClass('inactiveClass')
+      await page.goto(url + '/user/test', { waitUntil: 'networkidle' })
+      await expect(page.locator('a')).toHaveClass('inactiveClass')
+      await expect(page.locator('a')).not.toHaveClass('activeClass')
+    },
+  )
+})
