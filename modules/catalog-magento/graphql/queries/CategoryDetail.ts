219f3a591b09090fb728d23cce2d64c8a0247976
Jaroslav <jaroslavskudrna@gmail.com>
2022-12-07 12:31:07 +0100

    creating a simple product

--- modules/catalog-magento/graphql/queries/CategoryDetail.ts
+++ modules/catalog-magento/graphql/queries/CategoryDetail.ts
@@ -1,6 +1,6 @@
 import query from '#ioc/graphql/query'
 import field from '#ioc/graphql/field'
-import Product from '#ioc/graphql/fragments/Product'
+import ProductSimple from '#ioc/graphql/fragments/ProductSimple'
 import Aggregation from '#ioc/graphql/fragments/Aggregation'
 
 export default () =>
@@ -47,7 +47,7 @@ export default () =>
         .fields({
           total_count: field(),
           items: field({
-            ...Product(),
+            ...ProductSimple(),
           }),
         }),
       aggregations: field('products')
