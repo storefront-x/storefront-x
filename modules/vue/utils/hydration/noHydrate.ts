08dd86b500f86a4339a3dc7b6259478b1a5f4497
Jakub Rosina <32110080+kubo12312@users.noreply.github.com>
2022-12-09 13:10:40 +0100

    nohydrate utility

---
+++ modules/vue/utils/hydration/noHydrate.ts
@@ -0,0 +1,48 @@
+/* eslint-disable vue/one-component-per-file */
+import IS_SERVER from '#ioc/config/IS_SERVER'
+import { h, defineComponent, defineAsyncComponent, getCurrentInstance } from 'vue'
+
+const name = 'NoHydrate'
+
+export default (source: () => Promise<{ default: any }>): any => {
+  const EagerComponent = defineAsyncComponent(source)
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  let resolve: any
+
+  const promise = new Promise<any>((_resolve) => {
+    resolve = _resolve
+  })
+
+  const LazyComponent = defineAsyncComponent({
+    loader: () => promise,
+    suspensible: false,
+  })
+
+  if (IS_SERVER) {
+    return defineComponent({
+      name,
+      setup() {
+        return () => h(EagerComponent)
+      },
+    })
+  }
+
+  return defineComponent({
+    name,
+    setup() {
+      const currentInstance = getCurrentInstance()
+
+      // el is directly present in the setup function only during hydration
+      // this might be a case of abusing implementation details but it kinda makes sense?
+      const el = currentInstance!.vnode.el as Element | null
+
+      const isHydration = !!el
+      if (isHydration) {
+        return () => h(LazyComponent)
+      } else {
+        return () => h(EagerComponent)
+      }
+    },
+  })
+}
