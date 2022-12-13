4548db3ae2b72ca278afe7c0d6662868a2cc3529
Jakub Rosina <32110080+kubo12312@users.noreply.github.com>
2022-12-13 15:14:20 +0100

    Fix for login in checkout

--- modules/checkout-magento/services/useLoginCustomerInCheckout.ts
+++ modules/checkout-magento/services/useLoginCustomerInCheckout.ts
@@ -4,6 +4,7 @@ import useGetCustomer from '#ioc/services/useGetCustomer'
 import useCustomerStore from '#ioc/stores/useCustomerStore'
 import useMergeCarts from '#ioc/services/useMergeCarts'
 import MAGENTO_CUSTOMER_COOKIE_NAME from '#ioc/config/MAGENTO_CUSTOMER_COOKIE_NAME'
+import useCustomerMagentoStore from '#ioc/stores/useCustomerMagentoStore'
 
 export default () => {
   const loginCustomerRepository = useLoginCustomerRepository()
@@ -11,12 +12,14 @@ export default () => {
   const customerStore = useCustomerStore()
   const mergeCarts = useMergeCarts()
   const cookies = useCookies()
+  const customerMagentoStore = useCustomerMagentoStore()
 
   return async (email: string, password: string) => {
-    const { id } = await loginCustomerRepository(email, password)
-    cookies.set(MAGENTO_CUSTOMER_COOKIE_NAME, id, { path: '/' })
+    const { token } = await loginCustomerRepository(email, password)
+    customerMagentoStore.$patch({ customerId: token })
+    cookies.set(MAGENTO_CUSTOMER_COOKIE_NAME, token, { path: '/' })
     await mergeCarts()
-    const { customer } = await getCustomer(id)
+    const { customer } = await getCustomer()
     customerStore.$patch({ customer })
   }
 }
