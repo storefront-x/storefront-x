40b07a0e57549961ac62986d1ceaf4ef062460b1
Adam Jedlička <mail@adamjedlicka.cz>
2022-12-13 16:22:12 +0100

    Remove timeout

--- modules/solid/base/templates/entry.server.tsx
+++ modules/solid/base/templates/entry.server.tsx
@@ -4,9 +4,7 @@ import App from '~/.sfx/App'
 const hydrationScript = generateHydrationScript()
 
 export default async (ctx: any) => {
-  const rendered = await renderToStringAsync(() => <App />, {
-    timeoutMs: 1000,
-  })
+  const rendered = await renderToStringAsync(() => <App />)
 
   ctx.out.html = (html: string) =>
     html.replace('<div id="app"></div>', `<div id="app">${rendered}</div>${hydrationScript}`)
