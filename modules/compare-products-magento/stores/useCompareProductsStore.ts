f187db2ca4c9c2bcf10af8d6d009480e47b835fc
Jaroslav <jaroslavskudrna@gmail.com>
2022-12-19 21:22:13 +0100

    cookie logic

--- modules/compare-products-magento/stores/useCompareProductsStore.ts
+++ modules/compare-products-magento/stores/useCompareProductsStore.ts
@@ -4,8 +4,10 @@ import IS_CLIENT from '#ioc/config/IS_CLIENT'
 import COMPARE_PRODUCTS_COOKIE_NAME from '#ioc/config/COMPARE_PRODUCTS_COOKIE_NAME'
 import useCookies from '#ioc/composables/useCookies'
 import waitForStore from '#ioc/utils/vuePinia/waitForStore'
-import useGetCompareListByIdRepository from '#ioc/repositories/useCreateCompareListRepository'
+import useCreateCompareListIdRepository from '#ioc/repositories/useCreateCompareListIdRepository'
 import useCustomerStore from '#ioc/stores/useCustomerStore'
+import useGetCompareListById from '#ioc/services/useGetCompareListById'
+
 export default defineStore('compareProducts', {
   state: () => ({
     items: [] as ReturnType<typeof ToWishlistItem>[],
@@ -15,20 +17,24 @@ export default defineStore('compareProducts', {
     async serverInit() {
       if (IS_CLIENT) return
       const customerStore = useCustomerStore()
-      const getCompareListByIdRepository = useGetCompareListByIdRepository()
-      // const { id } = await getCompareListByIdRepository()
-      // console.log('testing compare store', id)
-      // const cookies = useCookies()
-      // const itemsCompared = cookies.get(COMPARE_PRODUCTS_COOKIE_NAME) || []
-      // this.$patch({ items: itemsCompared })
+      const createCompareListIdRepository = useCreateCompareListIdRepository()
+      const getCompareListById = useGetCompareListById()
+      const cookies = useCookies()
+
+      const { id } = await createCompareListIdRepository()
+      this.$patch({ compareListId: id })
+      const itemsCompared = cookies.get(COMPARE_PRODUCTS_COOKIE_NAME) || []
+      this.$patch({ items: itemsCompared })
+
       return await waitForStore(
         customerStore,
-        () => customerStore !== undefined, // just check the store
+        () => customerStore !== undefined,
         async () => {
           if (customerStore.customer) {
-            const { id } = await getCompareListByIdRepository()
-            this.$patch({ compareListId: id })
-            //find and patch items, add to a cookie
+            const { id } = await createCompareListIdRepository()
+            const { items } = await getCompareListById(id)
+            this.$patch({ items: items })
+            cookies.set(COMPARE_PRODUCTS_COOKIE_NAME, items, { path: '/' })
           }
         },
       )
