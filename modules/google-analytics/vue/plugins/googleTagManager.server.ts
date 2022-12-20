cadce2dd0802a5d922fdef533f77f8763a62af1b
JanVanekRelitas <janvanek@relitas.cz>
2022-12-20 17:30:12 +0100

    to deploy

---
+++ modules/google-analytics/vue/plugins/googleTagManager.server.ts
@@ -0,0 +1,15 @@
+import type { App } from 'vue'
+import GOOGLE_TAG_MANAGER_ID from '#ioc/config/GOOGLE_TAG_MANAGER_ID'
+import GOOGLE_TAG_MANAGER_SCRIPT_TYPE from '#ioc/config/GOOGLE_TAG_MANAGER_SCRIPT_TYPE'
+
+export const after = async (app: App, ctx?: any) => {
+  ctx.out.googleTagManager = (html: string) =>
+    html.replace(
+      '<head>',
+      `<head>\n<script type='${GOOGLE_TAG_MANAGER_SCRIPT_TYPE}'>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','${GOOGLE_TAG_MANAGER_ID}');</script>`,
+    )
+}
