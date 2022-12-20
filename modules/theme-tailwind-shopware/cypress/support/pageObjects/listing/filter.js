953ca472f6dc88d6a62bf591d898062b14b3ade2
Robin Vyslouzil <extVyslouzil@dr-max.global>
2022-12-20 15:05:26 +0100

    Add support for Listing

---
+++ modules/theme-tailwind-shopware/cypress/support/pageObjects/listing/filter.js
@@ -0,0 +1,14 @@
+export default function filter() {
+  cy.get('[data-cy=product-title]')
+    .invoke('text')
+    .then((before) => {
+      cy.get('[data-cy=filter-option]').last().click()
+
+      cy.waitUntil(() =>
+        cy
+          .get('[data-cy=product-title]')
+          .invoke('text')
+          .then((after) => before !== after),
+      )
+    })
+}
