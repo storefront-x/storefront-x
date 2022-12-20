9869213e721da61dbf9be7bdd228dc8155c01a13
Robin Vyslouzil <extVyslouzil@dr-max.global>
2022-12-20 14:22:25 +0100

    Fix wishlist file after rebase

--- modules/create-storefront-x/template/magento/modules/demo-magento/cypress/integration/wishlist.js
+++ modules/create-storefront-x/template/magento/modules/demo-magento/cypress/integration/wishlist.js
@@ -1,22 +1,14 @@
-<<<<<<< HEAD
 import expectMicrowishlistQuantity from '~/cypress/support/pageObjects/Base/expectMicrowishlistQuantity'
 import gotoWishlist from '~/cypress/support/pageObjects/Base/gotoWishlist'
-=======
-import expectMicrowishlistQuantity from '~/cypress/support/pageObjects/base/expectMicrowishlistQuantity'
-import gotoWishlist from '~/cypress/support/pageObjects/base/gotoWishlist'
-
->>>>>>> 8f6b357 (Add refactor for Base module)
-import Wishlist from '~/cypress/support/pageObjects/Wishlist'
-import Product from '~/cypress/support/pageObjects/Product'
+import expectWishlistQuantity from '~/cypress/support/pageObjects/wishlist/expectWishlistQuantity'
 import AccountCredentials from '~/cypress/support/pageObjects/account/accountCredentials'
 import register from '~/cypress/support/pageObjects/account/register'
 import login from '~/cypress/support/pageObjects/account/login'
 import logout from '~/cypress/support/pageObjects/account/logout'
 
-describe('Wishlist', () => {
-  /** @type {Wishlist} */
-  let wishlist = null
+import Product from '~/cypress/support/pageObjects/Product'
 
+describe('Wishlist', () => {
   /** @type {Product} */
   let product = null
 
@@ -28,7 +20,6 @@ describe('Wishlist', () => {
   })
 
   beforeEach(() => {
-    wishlist = new Wishlist()
     product = new Product()
   })
 
@@ -36,48 +27,31 @@ describe('Wishlist', () => {
     product.visitRandom()
     product.addToWishlist()
 
-<<<<<<< HEAD
-    expectMicrowishlistQuantity.expectMicrowishlistQuantity(1)
-    gotoWishlist.gotoWishlist()
-=======
     expectMicrowishlistQuantity(1)
     gotoWishlist()
->>>>>>> 8f6b357 (Add refactor for Base module)
 
-    wishlist.expectWishlistQuantity(1)
+    expectWishlistQuantity(1)
   })
 
   it('allows removing from the wishlist', () => {
     product.visitRandom()
     product.addToWishlist()
 
-<<<<<<< HEAD
-    expectMicrowishlistQuantity.expectMicrowishlistQuantity(1)
-=======
     expectMicrowishlistQuantity(1)
->>>>>>> 8f6b357 (Add refactor for Base module)
 
     product.visitRandom()
     product.addToWishlist()
 
-<<<<<<< HEAD
-    gotoWishlist.gotoWishlist()
-=======
     gotoWishlist()
->>>>>>> 8f6b357 (Add refactor for Base module)
 
-    wishlist.expectWishlistQuantity(2)
+    expectWishlistQuantity(2)
 
     product.visitAgain()
     product.getAddToWishlist().click() // Clicking again will remove it from wishlist
 
-<<<<<<< HEAD
-    gotoWishlist.gotoWishlist()
-=======
     gotoWishlist()
->>>>>>> 8f6b357 (Add refactor for Base module)
 
-    wishlist.expectWishlistQuantity(1)
+    expectWishlistQuantity(1)
   })
 
   it('merges wishlist with customer wishlist', () => {
@@ -96,12 +70,8 @@ describe('Wishlist', () => {
 
     login.login(accountCredentials)
 
-<<<<<<< HEAD
-    gotoWishlist.gotoWishlist()
-=======
     gotoWishlist()
->>>>>>> 8f6b357 (Add refactor for Base module)
 
-    wishlist.expectWishlistQuantity(3)
+    expectWishlistQuantity(3)
   })
 })
