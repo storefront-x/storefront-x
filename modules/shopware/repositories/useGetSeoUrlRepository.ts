4e6d86a639cea0e82c7bcdb8c4996f6544fad090
JanVanekRelitas <janvanek@relitas.cz>
2022-12-27 13:48:08 +0100

    done

--- modules/shopware/repositories/useGetSeoUrlRepository.ts
+++ modules/shopware/repositories/useGetSeoUrlRepository.ts
@@ -8,37 +8,44 @@ export default () => {
     if (path === '' || path === '/' || path === 'undefined') {
       path = 'home'
     }
-
+    const pathForeignKey = path.includes('p/') ? path.replace(/p\//, '') : null
+    const queries = [
+      {
+        type: 'equals',
+        field: 'seoPathInfo',
+        value: path,
+      },
+      {
+        type: 'equals',
+        field: 'seoPathInfo',
+        value: path + '/',
+      },
+      {
+        type: 'suffix',
+        field: 'pathInfo',
+        value: path,
+      },
+    ]
+    if (pathForeignKey) {
+      queries.push({
+        type: 'equals',
+        field: 'foreignKey',
+        value: pathForeignKey,
+      })
+    }
     const { elements }: any = await shopware.post('/seo-url', {
       filter: [
         {
           type: 'multi',
           operator: 'or',
-          queries: [
-            {
-              type: 'equals',
-              field: 'seoPathInfo',
-              value: path,
-            },
-            {
-              type: 'equals',
-              field: 'seoPathInfo',
-              value: path + '/',
-            },
-            {
-              type: 'suffix',
-              field: 'pathInfo',
-              value: path,
-            },
-          ],
+          queries: queries,
         },
       ],
     })
-
     return {
       foreignKey: elements[0]?.foreignKey as string,
       ident: elements[0]?.routeName as string,
-      seoPath: elements[0]?.seoPathInfo === 'home' ? '' : (('/' + elements[0].seoPathInfo) as string),
+      seoPath: elements[0]?.seoPathInfo === 'home' ? '/' : (('/' + elements[0].seoPathInfo) as string),
     }
   }
 }
--- modules/shopware/services/useUrlResolver.ts
+++ modules/shopware/services/useUrlResolver.ts
@@ -3,21 +3,26 @@ import useResource from '#ioc/composables/useResource'
 import arePathsSimilar from '#ioc/utils/url/arePathsSimilar'
 import dynamicPages from '~/.sfx/shopware/dynamicPages'
 import useRoute from '#ioc/composables/useRoute'
+import redirect from '#ioc/utils/redirect'
+import useLocalePath from '#ioc/composables/useLocalePath'
 
 export default () => {
   const getSeoUrlRepository = useGetSeoUrlRepository()
   const route = useRoute()
+  const localePath = useLocalePath()
 
   return async (resolvePath: string) => {
     const [data] = await useResource(
       () => resolvePath,
       (routePath) => getSeoUrlRepository(routePath),
     )
-
-    return {
-      id: data.value?.foreignKey,
-      component: dynamicPages[data.value?.ident as keyof typeof dynamicPages],
-      redirectTo: arePathsSimilar(route.path, data.value?.seoPath) ? null : data.value?.seoPath,
+    if (arePathsSimilar(route.path, data.value.seoPath)) {
+      return {
+        id: data.value?.foreignKey,
+        component: dynamicPages[data.value?.ident as keyof typeof dynamicPages],
+      }
+    } else {
+      redirect(localePath(data.value.seoPath), 301)
     }
   }
 }
