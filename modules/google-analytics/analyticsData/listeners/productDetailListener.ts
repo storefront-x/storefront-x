ba86032bd8bd0253e3dafaeecee56ec077312789
Damian Dlugosch <damian@damiandlugosch.com>
2023-01-02 15:03:18 +0100

    Bug fixes

--- modules/google-analytics/analyticsData/listeners/productDetailListener.ts
+++ modules/google-analytics/analyticsData/listeners/productDetailListener.ts
@@ -4,15 +4,15 @@ interface ProductItem {
   items: {
     item_id: string
     item_name: string
-    affiliation?: string
-    discount?: number
+    affiliation: string
+    discount: number
     item_brand?: string
-    item_category?: string
-    item_category2?: string
-    item_category3?: string
-    item_category4?: string
-    item_category5?: string
-    price?: number
+    item_category: string
+    item_category2: string
+    item_category3: string
+    item_category4: string
+    item_category5: string
+    price: number
     product_type: string
   }[]
 }
--- modules/theme-tailwind/templates/ProductDetail.vue
+++ modules/theme-tailwind/templates/ProductDetail.vue
@@ -70,15 +70,15 @@ onMounted(() =>
         discount:
           product.finalPrice?.value !== product.regularPrice?.value
             ? (+product.regularPrice.value - +product.finalPrice.value) / PRICE_OFFSET
-            : 0.0,
-        item_brand: productBrand?.valueLabel,
-        item_category: product.categories?.at(0)?.name,
-        item_category2: product.categories?.at(1)?.name,
-        item_category3: product.categories?.at(2)?.name,
-        item_category4: product.categories?.at(3)?.name,
-        item_category5: product.categories?.at(4)?.name,
+            : 0,
+        item_brand: productBrand?.valueLabel ?? '',
+        item_category: product.categories?.at(0)?.name ?? '',
+        item_category2: product.categories?.at(1)?.name ?? '',
+        item_category3: product.categories?.at(2)?.name ?? '',
+        item_category4: product.categories?.at(3)?.name ?? '',
+        item_category5: product.categories?.at(4)?.name ?? '',
         price: +product.regularPrice.value / PRICE_OFFSET,
-        product_type: product.productType,
+        product_type: product.productType ?? '',
       },
     ],
   }),
