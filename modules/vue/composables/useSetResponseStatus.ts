664722cdca1675652fece55e79a0b7e6ff9df766
Adam Jedlička <mail@adamjedlicka.cz>
2023-01-02 10:20:13 +0100

    Refactor

--- modules/vue/composables/useSetResponseStatus.ts
+++ modules/vue/composables/useSetResponseStatus.ts
@@ -1,13 +1,16 @@
 import useContext from '#ioc/composables/useContext'
 import IS_CLIENT from '#ioc/config/IS_CLIENT'
 
-export default () => {
+type SetResponseStatus = (status: number) => void
+
+export default (): SetResponseStatus => {
   if (IS_CLIENT) {
     return () => undefined
   }
+
   const ctx = useContext()
 
-  return async (status: number) => {
+  return (status: number) => {
     ctx.responseStatus = status
   }
 }
