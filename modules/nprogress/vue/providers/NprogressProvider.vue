5539533c235f7b0b108f8d147d870db837ea4c2a
Adam Jedlička <mail@adamjedlicka.cz>
2023-01-03 10:34:56 +0100

    Use TypeScript for IoC generated files (#247)

--- modules/nprogress/vue/providers/NprogressProvider.vue
+++ modules/nprogress/vue/providers/NprogressProvider.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script setup lang="ts">
-import useEventBus from '#ioc/composables/useEventBus.js'
+import useEventBus from '#ioc/composables/useEventBus'
 import NPROGRESS_BAR_TRICKLE_SPEED from '#ioc/config/NPROGRESS_BAR_TRICKLE_SPEED'
 import NProgress from 'nprogress'
 
