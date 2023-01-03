9ccf9d991af607c8c57105c0bcfa00dec3c1205a
Robin Vyslouzil <extVyslouzil@dr-max.global>
2023-01-03 10:38:23 +0100

    Fix category test for shopware

--- modules/demo-shopware/cypress/integration/category.js
+++ modules/demo-shopware/cypress/integration/category.js
@@ -1,20 +1,21 @@
 import visitCategory from '~/cypress/support/pageObjects/category/visitCategory'
 import continueShopping from '~/cypress/support/pageObjects/base/continueShopping'
 import expectMicrocartQuantity from '~/cypress/support/pageObjects/base/expectMicrocartQuantity'
-import getCategoryTitle from '~/cypress/support/pageObjects/base/getCategoryTitle'
+import getCategoryTitle from '~/cypress/support/pageObjects/category/getCategoryTitle'
 import sortByPrice from '~/cypress/support/pageObjects/listing/sortByPrice'
 import filter from '~/cypress/support/pageObjects/listing/filter'
 import getFirstAddToCart from '~/cypress/support/pageObjects/listing/getFirstAddToCart'
 
+let categoryName
+
 describe('Category', () => {
   beforeEach(() => {
-    visitCategory('Office').as('category')
+    categoryName = 'Office'
+    visitCategory(categoryName)
   })
 
   it('contains category title', () => {
-    cy.get('@category').then((category) => {
-      getCategoryTitle().will('include.text', () => category.name)
-    })
+    getCategoryTitle().will('include.text', () => categoryName)
   })
 
   it('can be sorted by prices in ascending order', () => {
