9b5c9a0487942fce37e789f1ada40ec097d67561
Adam Jedlička <mail@adamjedlicka.cz>
2023-01-04 15:26:30 +0100

    Add ToMagentoImage mapper

--- modules/blog-magento/mappers/ToBlogPost.ts
+++ modules/blog-magento/mappers/ToBlogPost.ts
@@ -1,3 +1,4 @@
+import ToMagentoImage from '#ioc/mappers/ToMagentoImage'
 import toDate from '#ioc/utils/date/toDate'
 import sanitizePageBuilderContent from '#ioc/utils/magento/sanitizePageBuilderContent'
 
@@ -9,8 +10,8 @@ export default (data: any) => ({
   status: data.status ?? '',
   fullContent: { content: sanitizePageBuilderContent(data.full_content) },
   publishedAt: toDate(data.published_at),
-  postThumbnail: useOriginalImage(data.post_thumbnail ?? ''),
-  listThumbnail: useOriginalImage(data.list_thumbnail ?? ''),
+  postThumbnail: ToMagentoImage(useOriginalImage(data.post_thumbnail ?? '')),
+  listThumbnail: ToMagentoImage(useOriginalImage(data.list_thumbnail ?? '')),
   meta: {
     title: data.meta_title ?? '',
     description: data.meta_description ?? '',
