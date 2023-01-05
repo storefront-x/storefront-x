38e4fa92e4deb89ecd307f759ffcd7eb6ec07cdd
Robin Vyslouzil <extVyslouzil@dr-max.global>
2023-01-05 15:14:43 +0100

    Change beforeEach for blog

--- modules/create-storefront-x/template/px/modules/demo-px/cypress/integration/blogCategory.js
+++ modules/create-storefront-x/template/px/modules/demo-px/cypress/integration/blogCategory.js
@@ -1,21 +1,24 @@
 import getBlogTitle from '~/cypress/support/pageObjects/blogCategory/getBlogTitle'
 import getBlogPostTiles from '~/cypress/support/pageObjects/blogCategory/getBlogPostTiles'
-import visitRandomBlogCategory from '~/cypress/support/pageObjects/blogCategory/visitRandomBlogCategory'
+import GetRandomBlogCategory from '~/cypress/support/repositories/GetRandomBlogCategory'
 
 describe('Blog category', () => {
   beforeEach(() => {
-    visitRandomBlogCategory().as('blogCategory')
+    GetRandomBlogCategory().as('blogCategory')
+    cy.get('@blogCategory').then((blogCategory) => {
+      cy.visit('/blog/category/' + blogCategory.url_key).waitForSfx()
+    })
   })
 
   it('renders its name', () => {
     cy.get('@blogCategory').then((blogCategory) => {
-      getBlogTitle().will('include.text', () => blogCategory.data.name)
+      getBlogTitle().will('include.text', () => blogCategory.name)
     })
   })
 
   it('has blog posts', () => {
     cy.get('@blogCategory').then((blogCategory) => {
-      getBlogPostTiles().will('have.length.at.most', () => blogCategory.data.post_count)
+      getBlogPostTiles().will('have.length.at.most', () => blogCategory.post_count)
     })
   })
 })
