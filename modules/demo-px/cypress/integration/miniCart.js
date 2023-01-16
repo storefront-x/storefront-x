52ec77785352bb28cb3de0ec23cae7cd4bd7d177
Robin Vyslouzil <extVyslouzil@dr-max.global>
2023-01-16 14:43:32 +0100

    Fix tests

--- modules/demo-px/cypress/integration/miniCart.js
+++ modules/demo-px/cypress/integration/miniCart.js
@@ -1,32 +1,34 @@
-import MiniCart from '~/cypress/support/pageObjects/MiniCart'
+import openMinicart from '~/cypress/support/pageObjects/miniCart/openMinicart'
+import closeMinicart from '~/cypress/support/pageObjects/miniCart/closeMinicart'
+import checkEmptyMinicart from '~/cypress/support/pageObjects/miniCart/checkEmptyMinicart'
+import getProductFromMinicart from '~/cypress/support/pageObjects/miniCart/getProductFromMinicart'
+import addToCart from '~/cypress/support/pageObjects/product/addToCart'
+import visitRandom from '~/cypress/support/pageObjects/product/visitRandom'
+
 import Product from '~/cypress/support/pageObjects/Product'
 
 describe('Minicart', () => {
-  /** @type {MiniCart} */
-  let miniCart
-
   /** @type {Product} */
   let product
 
   beforeEach(() => {
-    miniCart = new MiniCart()
     product = new Product()
   })
 
   it('open and close minicart without product', () => {
     cy.visit('/').waitForSfx()
 
-    miniCart.open()
-    miniCart.isEmpty()
-    miniCart.close()
+    openMinicart()
+    checkEmptyMinicart()
+    closeMinicart()
   })
 
   it('open and close minicart with product', () => {
-    product.visitRandom()
-    product.addToCart()
+    visitRandom(product)
+    addToCart()
 
-    miniCart.open()
-    miniCart.getTitle().will('include.text', () => product.data.name)
-    miniCart.close()
+    openMinicart()
+    getProductFromMinicart().should('include.text', product.data.name)
+    closeMinicart()
   })
 })
