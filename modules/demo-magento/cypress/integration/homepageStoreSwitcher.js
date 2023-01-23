f921ba0b4c67f2c219bd67f9d59383abe5023872
Robin Vyslouzil <extVyslouzil@dr-max.global>
2023-01-23 09:30:25 +0100

    WIP

--- modules/demo-magento/cypress/integration/homepageStoreSwitcher.js
+++ modules/demo-magento/cypress/integration/homepageStoreSwitcher.js
@@ -1,11 +1,48 @@
 import selectDifferentStore from '~/cypress/support/pageObjects/storeSwitcher/selectDifferentStore'
+import Product from '~/cypress/support/pageObjects/product/Product'
+import visitRandom from '~/cypress/support/pageObjects/product/visitRandom'
 
 describe('HomepageStoreSwitcher', () => {
+  let product = null
+
   beforeEach(() => {
+    product = new Product()
     cy.visit('/')
   })
 
   it('switches stores', () => {
     selectDifferentStore()
   })
+
+  it('change locale and check url', () => {
+    cy.url().should('not.include', /\/[a-z]{2}\//gm)
+
+    selectDifferentStore()
+
+    cy.url().should('include', /\/[a-z]{2}\//gm)
+    visitRandom(product)
+    cy.url().should('include', /\/[a-z]{2}\//gm + `/${product.data.url_key}`)
+  })
 })
+
+//get random product
+//store products url value
+//visit and check url
+//go to homepage
+//change language
+//check url for regexp `\/[a-z](2)\/`
+//visit stored url value with locale prefix
+//check that url contains locale prefix
+
+// describe('Locale', () => {
+//   let product = null
+
+//   beforeEach(() => {
+//     product = new Product()
+//     cy.visit('/').waitForSfx()
+//   })
+
+//   it('change locale and check url', () => {
+//     cy.url().should('not.include', /\/[a-z]{2}\//gm)
+//   })
+// })
--- modules/demo-px/cypress/integration/homepageStoreSwitcher.js
+++ modules/demo-px/cypress/integration/homepageStoreSwitcher.js
@@ -1,11 +1,26 @@
 import selectDifferentStore from '~/cypress/support/pageObjects/storeSwitcher/selectDifferentStore'
+import Product from '~/cypress/support/pageObjects/product/Product'
+import visitRandom from '~/cypress/support/pageObjects/product/visitRandom'
 
 describe('HomepageStoreSwitcher', () => {
+  let product = null
+
   beforeEach(() => {
+    product = new Product()
     cy.visit('/')
   })
 
   it('switches stores', () => {
     selectDifferentStore()
+  })
+
+  it.only('change locale and check url', () => {
+    cy.url().should('not.include', /\/[a-z]{2}\//)
+
+    selectDifferentStore()
+
+    cy.url().should('match', /\/[a-z]{2}\//)
+    visitRandom(product)
+    cy.url().should('match', /\/[a-z]{2}\//gm + `${product.url_key}`)
   })
 })
