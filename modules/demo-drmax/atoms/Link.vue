1e5455751d368dc09af00fa1600c64c10fcb407d
rubesovm <rubesovm@gmail.com>
2023-01-23 19:04:19 +0100

    Overriden megamentu and added styling

---
+++ modules/demo-drmax/atoms/Link.vue
@@ -0,0 +1,29 @@
+<template>
+  <Component :is="component" :class="classes">
+    <slot />
+  </Component>
+</template>
+
+<script setup lang="ts">
+import RouterLink from '#ioc/components/RouterLink'
+import { computed, PropType, useAttrs } from 'vue'
+
+const props = defineProps({
+  color: {
+    type: String as PropType<'primary' | 'gray' | 'black' | 'white' | 'blue'>,
+    default: 'primary',
+  },
+})
+
+const attrs = useAttrs()
+
+const component = computed(() => (attrs.to ? RouterLink : 'a'))
+
+const classes = computed(() => ({
+  'text-primary-500 hover:text-primary-500': props.color === 'primary',
+  'text-gray-600 hover:text-gray-800': props.color === 'gray',
+  'text-gray-900': props.color === 'black',
+  'text-white': props.color === 'white',
+  'text-blue-560': props.color === 'blue',
+}))
+</script>
---
+++ modules/demo-drmax/molecules/HeaderMenu/FlyoutMenuSubCategoryChild.vue
@@ -0,0 +1,24 @@
+<template>
+  <Link :to="localePath(category.urlPath)" color="black" class="font-normal hover:underline">
+    {{ category.name }}
+  </Link>
+</template>
+
+<script setup lang="ts">
+import Link from '#ioc/atoms/Link'
+import useCategory from '#ioc/composables/useCategory'
+import ToCategory from '#ioc/mappers/ToCategory'
+import { PropType, computed } from 'vue'
+import useLocalePath from '#ioc/composables/useLocalePath'
+
+const localePath = useLocalePath()
+
+const props = defineProps({
+  category: {
+    type: Object as PropType<ReturnType<typeof ToCategory>>,
+    required: true,
+  },
+})
+
+const category = useCategory(computed(() => props.category))
+</script>
