8efe4a6bb76e70318d7cc917d90d655658d74665
Jakub Rosina <32110080+kubo12312@users.noreply.github.com>
2023-01-31 18:06:59 +0100

    Product alerts components

---
+++ modules/theme-tailwind-magento-px/molecules/ProductAlertModal.vue
@@ -0,0 +1,65 @@
+<template>
+  <Modal @close="emit('close')">
+    <Heading :level="3" class="text-center">{{ t('Nastavit hlídání cen.') }}</Heading>
+    <p class="text-left mb-3">{{ t('Chci být informován') }}</p>
+    <SfxForm @submit="onSubmit">
+      <FormCheckbox
+        v-if="!product.available"
+        name="isAvailable"
+        class="mb-3"
+        :label="t('When the product is in stock')"
+      />
+
+      <FormCheckbox name="isPriceLower" class="mb-3" :label="t('When the price drops below')" />
+
+      <FormInput name="price" type="number" class="w-full text-left shadow-transparent border-0 mb-3" />
+
+      <FormInput
+        name="email"
+        type="email"
+        :label="t('Email')"
+        autocomplete="email"
+        validators="required|email"
+        class="text-left"
+      />
+
+      <div class="flex justify-end gap-4 mt-4">
+        <Button type="button" outline @click="onClose">{{ t('Close') }}</Button>
+        <Button type="submit" color="primary" data-cy="save">{{ t('Save') }}</Button>
+      </div>
+    </SfxForm>
+  </Modal>
+</template>
+
+<script setup lang="ts">
+import Modal from '#ioc/atoms/Modal'
+import Heading from '#ioc/atoms/Heading'
+import Button from '#ioc/atoms/Button'
+import FormInput from '#ioc/molecules/FormInput'
+import useI18n from '#ioc/composables/useI18n'
+import FormCheckbox from '#ioc/molecules/FormCheckbox'
+import SfxForm from '#ioc/components/SfxForm'
+import injectProduct from '#ioc/composables/injectProduct'
+
+const { t } = useI18n()
+const product = injectProduct()
+
+const emit = defineEmits(['create', 'close'])
+
+const onSubmit = (data: any) => {
+  emit('create', { ...data })
+}
+
+const onClose = () => {
+  emit('close')
+}
+</script>
+
+<i18n lang="yaml">
+cs-CZ:
+  When the price drops below: Při snížení ceny pod
+  When the product is in stock: Když bude produkt na skladě
+  Save: Uložit
+  Close: Zavřít
+  Email: Email
+</i18n>
---
+++ modules/theme-tailwind-magento-px/molecules/ProductAlerts.vue
@@ -0,0 +1,28 @@
+<template>
+  <button
+    class="rounded-md flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100"
+    @click="onNew"
+  >
+    <OutlineBellRinging class="ml-2 mr-1 text-gray-400" /> {{ t('Alert') }}
+    <ProductAlertModal v-if="modalOpen" @close="onClose" />
+  </button>
+</template>
+
+<script setup lang="ts">
+import OutlineBellRinging from '#ioc/icons/OutlineBellRinging'
+import useI18n from '#ioc/composables/useI18n'
+import ProductAlertModal from '#ioc/molecules/ProductAlertModal'
+import { ref } from 'vue'
+
+const { t } = useI18n()
+
+const modalOpen = ref(false)
+
+const onClose = () => {
+  modalOpen.value = false
+}
+
+const onNew = () => {
+  modalOpen.value = true
+}
+</script>
