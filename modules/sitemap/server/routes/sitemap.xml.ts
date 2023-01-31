fa79007e70caa11142fdac78818998ebde70f71d
Jakub Rosina <32110080+kubo12312@users.noreply.github.com>
2023-01-31 17:32:53 +0100

    Add static sites to sitemap

--- modules/sitemap/server/routes/sitemap.xml.ts
+++ modules/sitemap/server/routes/sitemap.xml.ts
@@ -2,17 +2,21 @@ import SitemapRenderer from '../../SitemapRenderer.js'
 import modules from '~/.sfx/sitemap'
 import isEmpty from '#ioc/utils/isEmpty'
 import VUE_I18N_LOCALES from '#ioc/config/VUE_I18N_LOCALES'
+import { routes } from '~/.sfx/pages'
 
 export default async (req: any, res: any) => {
   const store = getStoreFromUrl(req.hostname, req.url, VUE_I18N_LOCALES)
+  const staticPages = getStaticPages()
 
-  const _urls = []
+  const _urls = [] as any
 
   for (const module of Object.values(modules)) {
     _urls.push(module({ store }))
   }
 
-  const urls = (await Promise.all(_urls)).flat()
+  const mergedUrls = _urls.concat(staticPages)
+
+  const urls = (await Promise.all(mergedUrls)).flat()
 
   const sitemap = new SitemapRenderer().withStore(store).withUrls(urls).withHostname(req.hostname).render()
 
@@ -34,4 +38,16 @@ const getIndexOfCodeFromUrl = (url: string, matches: any) => {
   }
 
   return 0
+}
+
+const getStaticPages = () => {
+  const newRoutes = routes.slice(1, -1)
+
+  const urls = []
+
+  for (const route of newRoutes) {
+    urls.push({ loc: route.readablePath })
+  }
+
+  return urls
 }
