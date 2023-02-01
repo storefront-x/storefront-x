29a667458c4f6534017da8be16651b42a6393486
Adam Jedlička <mail@adamjedlicka.cz>
2023-02-01 13:58:01 +0100

    Add docs

--- modules/cart/composables/useCart.ts
+++ modules/cart/composables/useCart.ts
@@ -1,29 +1,31 @@
 import useCartStore from '#ioc/stores/useCartStore'
-import { computed, reactive, ref, onMounted } from 'vue'
+import { computed, reactive, ref, onMounted, readonly } from 'vue'
 
 export default () => {
-  const isMounted = ref(false)
+  // Cart data are loaded on the client side. This can cause hydration mismatch (empty cart on server vs cart data on client).
+  // By guarding everything behind onMounted hook, we know that the current component was hydrated and can accept client data.
+  const wasHydrated = ref(false)
 
   onMounted(() => {
-    isMounted.value = true
+    wasHydrated.value = true
   })
 
   const cartStore = useCartStore()
 
   const isLoaded = computed(() => {
-    if (!isMounted.value) return false
+    if (!wasHydrated.value) return false
 
     return cartStore.cart !== undefined
   })
 
   const items = computed(() => {
-    if (!isMounted.value) return []
+    if (!wasHydrated.value) return []
 
     return cartStore.cart?.items ?? []
   })
 
   const itemsTotalQuantity = computed(() => {
-    if (!isMounted.value) return 0
+    if (!wasHydrated.value) return 0
 
     let itemsTotalQuantity = 0
 
@@ -35,36 +37,37 @@ export default () => {
   })
 
   const subtotalIncludingTax = computed(() => {
-    if (!isMounted.value) return null
+    if (!wasHydrated.value) return null
 
     return cartStore.cart?.prices.subtotalIncludingTax ?? null
   })
 
   const taxes = computed(() => {
-    if (!isMounted.value) return []
+    if (!wasHydrated.value) return []
 
     return cartStore.cart?.prices.taxes ?? []
   })
 
   const discounts = computed(() => {
-    if (!isMounted.value) return []
+    if (!wasHydrated.value) return []
 
     return cartStore.cart?.prices.discounts ?? []
   })
 
   const coupons = computed(() => {
-    if (!isMounted.value) return []
+    if (!wasHydrated.value) return []
 
     return cartStore.cart?.coupons ?? []
   })
 
   const grandTotal = computed(() => {
-    if (!isMounted.value) return null
+    if (!wasHydrated.value) return null
 
     return cartStore.cart?.prices.grandTotal ?? null
   })
 
   return reactive({
+    _wasHydrated: readonly(wasHydrated),
     isLoaded,
     items,
     itemsTotalQuantity,
