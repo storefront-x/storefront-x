c81591a1936f82f4ca5fa4243d713068579ce5f7
Jan Vaněk <55270236+JanVanekRelitas@users.noreply.github.com>
2023-02-01 13:52:36 +0100

    Add support for "get product by ID" query  (#305)

--- modules/cart-magento/mappers/ToCartDiscount.ts
+++ modules/cart-magento/mappers/ToCartDiscount.ts
@@ -2,5 +2,5 @@ import ToMoney from '#ioc/mappers/ToMoney'
 
 export default (data: any) => ({
   amount: ToMoney(data.amount),
-  label: data.label as string,
+  label: String(data.label ?? '').trim(),
 })
