d515fcf7dafa309c6f8094d8cd2e6b07b87c8a27
JanVanekRelitas <janvanek@relitas.cz>
2023-02-02 14:46:19 +0100

    fix shop

--- modules/theme-tailwind-shopware/molecules/MicroCompareProducts.vue
+++ modules/theme-tailwind-shopware/molecules/MicroCompareProducts.vue
@@ -1,9 +1,5 @@
 <template>
-  <div class="rounded-lg border-2 p-2 border-none border-gray-100 lg:border-solid hover:bg-gray-100">
-    <OutlineScale stroke-width="2" class="text-primary-500" />
-  </div>
+  <div class="rounded-lg border-2 p-2 border-none border-gray-100 lg:border-solid hover:bg-gray-100"></div>
 </template>
 
-<script setup lang="ts">
-import OutlineScale from '#ioc/icons/OutlineScale'
-</script>
+<script setup lang="ts"></script>
