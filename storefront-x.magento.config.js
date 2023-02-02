79fabd8abeb841329f761480400e2d988e61cea8
JanVanekRelitas <janvanek@relitas.cz>
2023-02-02 13:32:06 +0100

    fix to tests

--- storefront-x.magento.config.js
+++ storefront-x.magento.config.js
@@ -32,6 +32,7 @@ export default {
     '@storefront-x/customer-magento',
     '@storefront-x/multicurrency-magento',
     '@storefront-x/product-reviews-magento',
+    '@storefront-x/product-comparison-magento',
     '@storefront-x/wishlist-magento',
 
     '@storefront-x/theme-tailwind',
