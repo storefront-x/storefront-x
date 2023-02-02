645bf1830add9d4929cd7765dfda04a77f5492ef
Jakub Rosina <32110080+kubo12312@users.noreply.github.com>
2023-02-02 19:42:45 +0100

    Upgrade Cypress to v12.5.1

--- modules/cypress/cypress/plugins/vitePreprocessor.cjs
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const vitePreprocessor = require('cypress-vite')
-const path = require('node:path')
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-module.exports = (on, config) => {
-  on('file:preprocessor', vitePreprocessor(path.resolve(process.cwd(), 'viteCypress.config.js')))
-}
