c71c707f086add0415ac54e3cf2eade499a25f3d
Adam Jedlička <mail@adamjedlicka.cz>
2023-02-06 09:21:08 +0100

    Add support for types extensions

--- modules/demo-shopware/config/VUE_I18N_LOCALES.ts
+++ modules/demo-shopware/config/VUE_I18N_LOCALES.ts
@@ -1,10 +1,12 @@
-export default [
+import Locale from '#ioc/types/vue-i18n/Locale'
+
+const locales: Locale[] = [
   {
     fullName: 'English',
     name: 'en',
     locale: 'en-US',
     prefix: '/',
-    languageId: '2fbb5fe2e29a4d70aa5854ce7ce3e20b',
+    languageId: 'asd',
   },
   {
     fullName: 'Czech',
@@ -14,3 +16,5 @@ export default [
     languageId: '475ecdc28894466ba808268a33c37fcf',
   },
 ]
+
+export default locales
