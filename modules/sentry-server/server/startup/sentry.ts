e277ecf08ca5262bc45b86a42333d785d0425a51
Adam Jedlička <mail@adamjedlicka.cz>
2023-02-13 11:37:40 +0100

    Add option to set Sentry environment (#346)

--- modules/sentry-server/server/startup/sentry.ts
+++ modules/sentry-server/server/startup/sentry.ts
@@ -1,6 +1,7 @@
 import * as Sentry from '@sentry/node'
 import dsn from '#ioc/config/sentry/server/dsn'
 import tracesSampleRate from '#ioc/config/sentry/server/tracesSampleRate'
+import environment from '#ioc/config/sentry/server/environment'
 
 // Importing @sentry/tracing patches the global hub for tracing to work.
 import '@sentry/tracing'
@@ -8,4 +9,5 @@ import '@sentry/tracing'
 Sentry.init({
   dsn,
   tracesSampleRate,
+  environment,
 })
