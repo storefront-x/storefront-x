a5cc6816714c0cfef0903af011dcd78870be4e08
Damian Dlugosch <damian@damiandlugosch.com>
2023-02-14 14:40:09 +0100

    Add helper function to generate content in Tailwind config

--- modules/demo-shopware/tailwind.config.js
+++ modules/demo-shopware/tailwind.config.js
@@ -1,12 +1,31 @@
 import colors from 'tailwindcss/colors.js'
 import forms from '@tailwindcss/forms'
+import { readdir } from 'node:fs/promises'
+import { argv } from 'node:process'
+
+const getTailwindContent = async (source) => {
+  const runningConfig = argv.find((arg) => arg.startsWith('storefront-x'))?.split('.')[1] || ''
+  const dir = (await readdir(source, { withFileTypes: true })) || []
+  return dir
+    .filter((dirent) => dirent.name.match(/^(theme-tailwind|demo)/))
+    .filter((dirent) =>
+      runningConfig === 'px'
+        ? dirent.name === 'theme-tailwind' ||
+          dirent.name === 'theme-tailwind-magento' ||
+          dirent.name === `demo-${runningConfig}` ||
+          dirent.name.endsWith(runningConfig)
+        : dirent.name === 'theme-tailwind' ||
+          dirent.name === `demo-${runningConfig}` ||
+          dirent.name.endsWith(runningConfig),
+    )
+    .sort((a, b) => (a.name.includes('theme') && !b.name.includes('theme-') ? -1 : 1))
+    .map((dirent) => `${source}${dirent.name}/**/*.vue`)
+}
+
+const content = await getTailwindContent('./node_modules/@storefront-x/')
 
 export default {
-  content: [
-    './node_modules/@storefront-x/theme-tailwind/**/*.vue',
-    './node_modules/@storefront-x/theme-tailwind-shopware/**/*.vue',
-    './node_modules/@storefront-x/demo-shopware/**/*.vue',
-  ],
+  content,
   plugins: [forms],
   theme: {
     extend: {
