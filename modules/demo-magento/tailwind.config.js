015ba7d7b981ff89d9b479e4f9937c238512bf94
Damian Dlugosch <damian@damiandlugosch.com>
2023-02-15 14:32:08 +0100

    Code refactoring

--- modules/demo-magento/tailwind.config.js
+++ modules/demo-magento/tailwind.config.js
@@ -1,29 +1,9 @@
 import colors from 'tailwindcss/colors.js'
 import forms from '@tailwindcss/forms'
-import { readFile } from 'node:fs/promises'
-import { argv } from 'node:process'
+import useGetTailwindContent from '@storefront-x/theme-tailwind'
 
-const getTailwindContent = async (source) => {
-  const configIndex = argv.findIndex((arg) => arg === '--config')
-  const runningConfig = configIndex !== -1 ? argv[configIndex + 1] : 'storefront-x.config.js'
-  try {
-    const configFile = await readFile(`./${runningConfig}`, { encoding: 'utf8' })
-    const modules = configFile
-      ?.replace(/^export default\s*|\s/g, '')
-      ?.replaceAll(`'`, '')
-      ?.split('modules:[')[1]
-      ?.split(']')[0]
-      ?.split(',')
-      ?.filter(Boolean)
-      ?.map((module) => `${source}${module}/**/*.vue`)
-
-    return modules || []
-  } catch (e) {
-    return []
-  }
-}
-
-const content = await getTailwindContent('./node_modules/')
+const getTailwindContent = useGetTailwindContent()
+const content = await getTailwindContent()
 
 export default {
   content,
---
+++ modules/theme-tailwind/index.js
@@ -0,0 +1,19 @@
+import { argv } from 'node:process'
+
+export default () => {
+  return async () => {
+    const configIndex = argv.findIndex((arg) => arg === '--config')
+    const runningConfig = configIndex !== -1 ? argv[configIndex + 1] : 'storefront-x.config.js'
+    try {
+      const configFile = await import(`${process.cwd()}/${runningConfig}`)
+      const modules = Object.values(configFile)?.flatMap(({ modules }) =>
+        modules?.map((module) => `./node_modules/${module}/**/*.vue`),
+      )
+
+      return modules || []
+    } catch (e) {
+      console.error(e)
+      return []
+    }
+  }
+}
