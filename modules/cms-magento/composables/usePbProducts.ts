4878e1b7f0c60f1d5b0c4cf3bd3a8531ef7684ee
Martin Fajman <fajmanm@magexo.cz>
2023-02-16 23:10:17 +0100

    SFX-355: remove typing

--- modules/cms-magento/composables/usePbProducts.ts
+++ modules/cms-magento/composables/usePbProducts.ts
@@ -1,17 +1,13 @@
 import { computed, reactive } from 'vue'
 
 export default (el: any) => {
-  interface Type {
-    type: 'carousel' | 'grid'
-  }
-
   const skus = computed(() => {
     const els = Array.from(el.querySelectorAll('[data-product-sku]'))
 
     return els.map((form: any) => form.getAttribute('data-product-sku'))
   })
 
-  const type = computed<Type>(() => el.getAttribute('data-appearance'))
+  const type = computed(() => el.getAttribute('data-appearance'))
 
   return reactive({
     skus,
