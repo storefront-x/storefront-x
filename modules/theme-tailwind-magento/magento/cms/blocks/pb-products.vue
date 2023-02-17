69565a4fa0b5b8798cff86f174334dc01fcf2346
Martin Fajman <39699015+fajmanm@users.noreply.github.com>
2023-02-17 11:52:19 +0100

    Add support for Product Grid in PB product component (#358)
    Co-authored-by: Adam Jedlička <mail@adamjedlicka.cz>

--- modules/theme-tailwind-magento/magento/cms/blocks/pb-products.vue
+++ modules/theme-tailwind-magento/magento/cms/blocks/pb-products.vue
@@ -1,6 +1,15 @@
 <template>
   <ProductCarousel v-if="products.length" :products="products" />
+
+  <div v-if="pbProducts.type === 'carousel'">
+    <ProductCarousel v-if="products.length" :products="products" />
+  </div>
+
+  <div v-if="pbProducts.type === 'grid'">
+    <ProductGrid v-if="products.length" :products="products" />
+  </div>
 </template>
+
 <script setup lang="ts">
 import useGetProductsByIds from '#ioc/services/useGetProductsByIds'
 import usePbProducts from '#ioc/composables/usePbProducts'
@@ -9,6 +18,7 @@ import hydrateWhenVisible from '#ioc/utils/hydration/hydrateWhenVisible'
 import { computed } from 'vue'
 
 const ProductCarousel = hydrateWhenVisible(() => import('#ioc/organisms/ProductCarousel'))
+const ProductGrid = hydrateWhenVisible(() => import('#ioc/organisms/ProductGrid'))
 
 const props = defineProps({ el: { type: Object, default: null } })
 
@@ -19,8 +29,10 @@ const [data] = await useResource(
   () => pbProducts.skus,
   (skus) => getProductsByIds(skus),
 )
+
 const products = computed(() => data.value.products)
 </script>
+
 <script lang="ts">
 import { defineComponent } from 'vue'
 
