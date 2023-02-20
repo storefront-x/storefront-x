4a5fa5ae2bfaddc5ff36729c70f50df5d1d71959
Damian Dlugosch <damian@damiandlugosch.com>
2023-02-20 12:09:35 +0100

    Bug fixes

--- modules/core/src/Build.js
+++ modules/core/src/Build.js
@@ -98,7 +98,7 @@ export default class Build extends Core {
     const runtimeCaching = []
 
     try {
-      const { default: manifest } = await import('file://' + path.join(this.buildDir, 'ioc', 'sw', 'manifest.js'))
+      const { default: manifest } = await import(path.join(this.buildDir, 'ioc', 'sw', 'manifest.js'))
       if (!manifest) {
         consola.info('Manifest not found')
       } else {
@@ -110,9 +110,7 @@ export default class Build extends Core {
     }
 
     try {
-      const { default: runtimeCaches } = await import(
-        'file://' + path.join(this.buildDir, 'ioc', 'sw', 'runtimeCaches.js')
-      )
+      const { default: runtimeCaches } = await import(path.join(this.buildDir, 'ioc', 'sw', 'runtimeCaches.js'))
       runtimeCaching.push(...runtimeCaches)
     } finally {
       await generateSW({
