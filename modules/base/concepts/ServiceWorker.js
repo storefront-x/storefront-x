fc476cf96a895d4f8c442fdc73ee5dc801e80727
Damian Dlugosch <damian@damiandlugosch.com>
2023-02-20 11:59:40 +0100

    Bug fixes

--- modules/base/concepts/ServiceWorker.js
+++ modules/base/concepts/ServiceWorker.js
@@ -39,7 +39,7 @@ export default class ServiceWorker extends IocConcept {
   }
 
   getPathForFile(module, file) {
-    return 'file://' + module.join(this.directory, file).replace(/\.ts$/, '').replace(/\\/g, '/')
+    return module.join(this.directory, file).replace(/\.ts$/, '').replace(/\\/g, '/')
   }
 
   get directory() {
