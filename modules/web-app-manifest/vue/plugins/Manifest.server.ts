d724e81e51f3f3c002cfba55d78063f7de70b5c0
Martin Fajman <fajmanm@magexo.cz>
2023-02-24 21:48:11 +0100

    Fix manifest syntax error

--- modules/web-app-manifest/vue/plugins/Manifest.server.ts
+++ modules/web-app-manifest/vue/plugins/Manifest.server.ts
@@ -2,7 +2,7 @@ import type { App } from 'vue'
 import webAppManifest from '~/.sfx/webAppManifest'
 
 export const after = async (app: App, ctx?: any) => {
-  let content = `<link rel="manifest" href="/manifest.webmanifest">`
+  let content = `<link rel="manifest" crossorigin="use-credentials" href="/manifest.webmanifest">`
 
   if (webAppManifest.theme_color) {
     content += `<meta name="theme-color" content="${webAppManifest.theme_color}" />`
