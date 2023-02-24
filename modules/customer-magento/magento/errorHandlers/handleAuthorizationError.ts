50363072d76f9a5244dae7ca04736e9f9d8b1465
Adam Jedlička <mail@adamjedlicka.cz>
2023-02-24 14:37:05 +0100

    Move error handling to top level error handlers

--- modules/customer-magento/magento/errorHandlers/handleAuthorizationError.ts
+++ modules/customer-magento/magento/errorHandlers/handleAuthorizationError.ts
@@ -1,23 +1,9 @@
-import useCustomerToken from '#ioc/composables/useCustomerToken'
 import CustomerNotAuthorized from '#ioc/errors/CustomerNotAuthorized'
-import useCartStore from '#ioc/stores/useCartStore'
-import useCheckoutStore from '#ioc/stores/useCheckoutStore'
-import useCustomerStore from '#ioc/stores/useCustomerStore'
 import isAuthorizationError from '#ioc/utils/graphql/isAuthorizationError'
 
 export default () => {
-  const customerStore = useCustomerStore()
-  const cartStore = useCartStore()
-  const checkoutStore = useCheckoutStore()
-  const customerToken = useCustomerToken()
-
   return async (error: any) => {
     if (isAuthorizationError(error)) {
-      customerToken.remove()
-      customerStore.$patch({ customer: null })
-      cartStore.$reset()
-      checkoutStore.$reset()
-
       throw new CustomerNotAuthorized()
     }
   }
