f4e76836da689a7e1009ba1565a300de66b51d4f
Martin Fajman <fajmanm@magexo.cz>
2023-02-24 16:55:45 +0100

    Improve CMS block for slots, cms block error handling

--- modules/cms-magento/components/SfxMagentoCmsBlock.vue
+++ modules/cms-magento/components/SfxMagentoCmsBlock.vue
@@ -1,5 +1,9 @@
 <template>
-  <SfxMagentoCmsPage :cms-page="data.cmsBlock" />
+  <div v-if="cmsData">
+    <slot name="above" />
+    <SfxMagentoCmsPage :cms-page="data.cmsBlock" />
+    <slot name="bellow" />
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -16,5 +20,15 @@ const props = defineProps({
   },
 })
 
-const { data } = await useAsyncData(`getCMSBlock_${props?.identifier}`, () => getCmsBlock(props.identifier))
+let cmsData: Awaited<ReturnType<typeof getCmsBlock>> | null = null
+
+try {
+  const { data, error } = await useAsyncData(`getCMSBlock_${props?.identifier}`, () => getCmsBlock(props.identifier))
+  if (error.value) {
+    throw error.value
+  }
+  cmsData = data.value
+} catch (e) {
+  console.warn(e)
+}
 </script>
