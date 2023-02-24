7e306908c6abc3ff1cde23cbdc0d11144008e5d9
Adam Jedlička <mail@adamjedlicka.cz>
2023-02-24 15:12:04 +0100

    Move error handling to top level error handlers (#372)

--- modules/cart-magento/magento/errorHandlers/handleInvalidCart.ts
+++ modules/cart-magento/magento/errorHandlers/handleInvalidCart.ts
@@ -1,20 +1,11 @@
 import CartResetted from '#ioc/errors/CartResetted'
-import useCartStore from '#ioc/stores/useCartStore'
-import useCartToken from '#ioc/composables/useCartToken'
 
 export default () => {
-  const cartStore = useCartStore()
-  const cartToken = useCartToken()
-
   const errorMessages = ['Could not find a cart with ID', `The cart isn't active`]
 
   return async (error: any) => {
     for (const message of errorMessages) {
       if (error.message?.includes(message)) {
-        cartToken.remove()
-
-        cartStore.$reset()
-
         throw new CartResetted()
       }
     }
--- modules/theme-tailwind/vue/errorHandlers/handleCartResetted.ts
+++ modules/theme-tailwind/vue/errorHandlers/handleCartResetted.ts
@@ -1,13 +1,20 @@
 import CartResetted from '#ioc/errors/CartResetted'
 import useShowErrorNotification from '#ioc/composables/useShowErrorNotification'
 import useI18n from '#ioc/composables/useI18n'
+import useCartStore from '#ioc/stores/useCartStore'
+import useCartToken from '#ioc/composables/useCartToken'
 
 export default () => {
+  const cartStore = useCartStore()
+  const cartToken = useCartToken()
   const showErrorNotification = useShowErrorNotification()
   const { t } = useI18n()
 
   return (error: any) => {
     if (error instanceof CartResetted) {
+      cartToken.remove()
+      cartStore.$reset()
+
       showErrorNotification(t('errors.cartExpired'))
     } else {
       throw error
