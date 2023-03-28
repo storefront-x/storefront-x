b09fb7c8a85bdbbd89916eecaa5f869b00fa80c7
Martin Fajman <fajmanm@magexo.cz>
2023-03-28 17:46:48 +0200

    label fragment refactor

--- modules/catalog-labels-magento/graphql/fragments/ProductInListing.ext.ts
+++ modules/catalog-labels-magento/graphql/fragments/ProductInListing.ext.ts
@@ -1,17 +1,20 @@
 import field from '#ioc/graphql/field'
+import Extension from '#ioc/types/base/Extension'
+import addFields from '#ioc/utils/graphql/addFields'
 import ProductLabel from '#ioc/graphql/fragments/ProductLabel'
 
-export default (self: any) => {
-  return () => {
-    const fragment = self()
-    fragment.ProductInListing.fields({
-      product_labels: field({
-        items: field({
-          ...ProductLabel(),
-        }),
-      }),
-    })
+const ProductInListing: Extension = (ProductInListing) => () => {
+  const self = ProductInListing()
 
-    return fragment
-  }
+  addFields(self, {
+    product_labels: field({
+      items: field({
+        ...ProductLabel(),
+      }),
+    }),
+  })
+
+  return self
 }
+
+export default ProductInListing
