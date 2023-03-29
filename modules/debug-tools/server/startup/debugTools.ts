e07e03c99ccd02ccaa93e2995e2531c6d1986c0a
Adam Jedlička <mail@adamjedlicka.cz>
2023-03-29 11:44:33 +0200

    Update error message

--- modules/debug-tools/server/startup/debugTools.ts
+++ modules/debug-tools/server/startup/debugTools.ts
@@ -3,5 +3,7 @@ import { versions } from 'node:process'
 const majorNodeVersion = parseInt(versions.node.split('.')[0])
 
 if (majorNodeVersion < 18) {
-  throw new Error('You are running an old version of Node.js. Please upgrade to Node 18 or newer.')
+  throw new Error(
+    `Module @storefront-x/debug-tools requires Node.js version 18 or newer. You are using Node.js version ${versions.node}.`,
+  )
 }
