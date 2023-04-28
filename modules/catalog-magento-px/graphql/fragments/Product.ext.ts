037c08a9208c20e812125edbbe074275f8fa7d6e
JanVanekRelitas <janvanek@relitas.cz>
2023-04-28 10:20:41 +0200

    fix in Product.ext

--- modules/catalog-magento-px/graphql/fragments/Product.ext.ts
+++ modules/catalog-magento-px/graphql/fragments/Product.ext.ts
@@ -2,27 +2,30 @@ import field from '#ioc/graphql/field'
 import Extension from '#ioc/types/base/Extension'
 import addFields from '#ioc/utils/graphql/addFields'
 import Money from '#ioc/graphql/fragments/Money'
+import on from '#ioc/graphql/on'
 
 const Product: Extension = (Product) => () => {
   const self = Product()
 
   addFields(self, {
-    price_view: field(),
-    price_range: field({
-      minimum_price: field({
-        final_price: field({
-          ...Money(),
+    ...on('BundleProduct', {
+      price_view: field(),
+      price_range: field({
+        minimum_price: field({
+          final_price: field({
+            ...Money(),
+          }),
+          regular_price: field({
+            ...Money(),
+          }),
         }),
-        regular_price: field({
-          ...Money(),
-        }),
-      }),
-      maximum_price: field({
-        final_price: field({
-          ...Money(),
-        }),
-        regular_price: field({
-          ...Money(),
+        maximum_price: field({
+          final_price: field({
+            ...Money(),
+          }),
+          regular_price: field({
+            ...Money(),
+          }),
         }),
       }),
     }),
