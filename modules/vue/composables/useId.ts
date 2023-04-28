42fc521b480f35ea81f1fd8b21448a9d5d988c0d
Jan Vaněk <55270236+JanVanekRelitas@users.noreply.github.com>
2023-04-28 13:58:51 +0200

    Fixed useResource issue with doubling ident when called from siblings (#417)

--- modules/vue/composables/useId.ts
+++ modules/vue/composables/useId.ts
@@ -1,8 +1,12 @@
+import useContext from '#ioc/composables/useContext'
 import hashCode from '#ioc/utils/string/hashCode'
 import { getCurrentInstance } from 'vue'
 
 export default (): string => {
+  const ctx = useContext()
   const currentInstance = getCurrentInstance()
+
+  ctx.__useIdHashes ??= new Map()
 
   let ident = ''
   let instance = currentInstance
@@ -12,5 +16,13 @@ export default (): string => {
     instance = instance.parent
   }
 
-  return String(hashCode(ident))
+  const hash = String(hashCode(ident))
+
+  if (ctx.__useIdHashes.has(hash)) {
+    ctx.__useIdHashes.set(hash, ctx.__useIdHashes.get(hash) + 1)
+  } else {
+    ctx.__useIdHashes.set(hash, 0)
+  }
+
+  return `${hash}-${ctx.__useIdHashes.get(hash)}`
 }
