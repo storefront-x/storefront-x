3f334477514dd9e5429518a35995faee4f41ee22
JanVanekRelitas <janvanek@relitas.cz>
2023-04-28 13:49:19 +0200

    done

--- modules/customer-magento/composables/useCustomerToken.ts
+++ modules/customer-magento/composables/useCustomerToken.ts
@@ -1,6 +1,11 @@
 import useCookies from '#ioc/composables/useCookies'
 import useCurrentLocale from '#ioc/composables/useCurrentLocale'
 
+interface Options {
+  path?: string
+  expires?: Date | undefined
+}
+
 export default () => {
   const cookies = useCookies()
   const currentLocale = useCurrentLocale()
@@ -9,7 +14,7 @@ export default () => {
 
   return {
     get: () => cookies.get(ident),
-    set: (value: string) => cookies.set(ident, value, { path: '/' }),
+    set: (value: string, options: Options = {}) => cookies.set(ident, value, { path: '/', ...options }),
     remove: () => cookies.remove(ident),
   }
 }
