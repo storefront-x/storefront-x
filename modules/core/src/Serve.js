007e18c5fea7f4cbf3f83c24e6867e634c00e2df
Adam Jedlička <mail@adamjedlicka.cz>
2023-05-08 17:21:11 +0200

    Fix nested server routes

--- modules/core/src/Serve.js
+++ modules/core/src/Serve.js
@@ -109,7 +109,7 @@ export default class Serve extends Core {
 
     for (const [path, route] of Object.entries(routes)) {
       if (typeof route === 'function') {
-        router.use(`/${path.replace(/\[(.+?)\]/g, (_, $1) => `:${$1}`).replace(/\[\.\.\.\]/g, '*')}`, route)
+        router.use(`/${path.replace(/\[\.\.\.\]/g, '**').replace(/\[(.+?)\]/g, (_, $1) => `:${$1}`)}`, route)
       }
     }
 
