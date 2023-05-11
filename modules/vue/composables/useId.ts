3823ac96b6cacac0c90eca65e51edc2bf5abcb16
JanVanekRelitas <janvanek@relitas.cz>
2023-05-11 14:03:15 +0200

    fixed useresource client and server ident equal

--- modules/vue/composables/useId.ts
+++ modules/vue/composables/useId.ts
@@ -1,28 +1,17 @@
-import useContext from '#ioc/composables/useContext'
 import hashCode from '#ioc/utils/string/hashCode'
 import { getCurrentInstance } from 'vue'
 
-export default (): string => {
-  const ctx = useContext()
+export default (sourceData: any): string => {
   const currentInstance = getCurrentInstance()
 
-  ctx.__useIdHashes ??= new Map()
-
-  let ident = ''
   let instance = currentInstance
+  let ident = ''
+  const sourceIdent = sourceData ? JSON.stringify(sourceData) : ''
 
   while (instance) {
     ident += instance.type.name
     instance = instance.parent
   }
 
-  const hash = String(hashCode(ident))
-
-  if (ctx.__useIdHashes.has(hash)) {
-    ctx.__useIdHashes.set(hash, ctx.__useIdHashes.get(hash) + 1)
-  } else {
-    ctx.__useIdHashes.set(hash, 0)
-  }
-
-  return `${hash}-${ctx.__useIdHashes.get(hash)}`
+  return String(hashCode(ident + sourceIdent))
 }
