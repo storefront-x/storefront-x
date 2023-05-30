1b9503c89888bd1669eb36ac1b08cf90d76f8043
Adam Jedlička <mail@adamjedlicka.cz>
2023-05-30 18:39:44 +0200

    Add support for recursive global styles (#429)
    Closes #427

--- modules/base/tests/playwright/GlobalStyles.spec.js
+++ modules/base/tests/playwright/GlobalStyles.spec.js
@@ -32,8 +32,51 @@ test('global styles are injected into html', async ({ page }) => {
       ],
     },
     async ({ url }) => {
-      await page.goto(url, { waitUntil: 'networkidle' })
-      await expect(await page.content()).toContain('background-color:red')
+      const response = await page.goto(url, { waitUntil: 'networkidle' })
+      expect(await response.text()).toContain('background-color:red')
+    },
+  )
+})
+
+test('global styles can be nested', async ({ page }) => {
+  await makeProject(
+    {
+      modules: [
+        '@storefront-x/base',
+        '@storefront-x/vue',
+        '@storefront-x/vue-router',
+        [
+          'my-module',
+          {
+            global: {
+              styles: {
+                a: {
+                  b: {
+                    c: {
+                      'style.css': `
+                        body: {
+                          background-color:red;
+                        }
+                      `,
+                    },
+                  },
+                },
+              },
+            },
+            pages: {
+              'index.vue': `
+                <template>
+                  <h1>Hello, World!</h1>
+                </template>
+              `,
+            },
+          },
+        ],
+      ],
+    },
+    async ({ url }) => {
+      const response = await page.goto(url, { waitUntil: 'networkidle' })
+      expect(await response.text()).toContain('background-color:red')
     },
   )
 })
