691032581cfd8d2c15451902851acaff8403b6a7
JanVanekRelitas <janvanek@relitas.cz>
2023-06-19 18:05:23 +0200

    cookie-messages with notification

--- modules/customer-magento/magento/errorHandlers/handleAuthorizationError.ts
+++ modules/customer-magento/magento/errorHandlers/handleAuthorizationError.ts
@@ -1,9 +1,16 @@
 import CustomerNotAuthorized from '#ioc/errors/CustomerNotAuthorized'
 import isAuthorizationError from '#ioc/utils/graphql/isAuthorizationError'
+import useCookieMessage from '#ioc/composables/useCookieMessage'
 
 export default () => {
+  const cookieMessage = useCookieMessage()
   return async (error: any) => {
     if (isAuthorizationError(error)) {
+      cookieMessage.set({
+        type: 'ERROR',
+        title: 'Authorization error',
+        message: 'authorizationError',
+      })
       throw new CustomerNotAuthorized(error.message)
     }
   }
