68978bc1103c0d8f0ec20cec6a3304d4c5d35cf6
Jan Vaněk <55270236+JanVanekRelitas@users.noreply.github.com>
2023-06-26 15:51:07 +0200

    Fixed Wishlists query cantBeCached (#437)

--- modules/wishlist-magento/graphql/queries/Wishlists.ts
+++ modules/wishlist-magento/graphql/queries/Wishlists.ts
@@ -3,10 +3,12 @@ import Wishlist from '#ioc/graphql/fragments/Wishlist'
 import query from '#ioc/graphql/query'
 
 export default () =>
-  query({
-    customer: field({
-      wishlists: field({
-        ...Wishlist(),
+  query()
+    .cantBeCached()
+    .fields({
+      customer: field({
+        wishlists: field({
+          ...Wishlist(),
+        }),
       }),
-    }),
-  })
+    })
