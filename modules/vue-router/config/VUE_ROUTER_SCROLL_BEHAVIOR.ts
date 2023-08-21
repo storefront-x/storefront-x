ccdd0838bac9dafcd2c3c2affff70b0d790a7555
Adam Jedlička <mail@adamjedlicka.cz>
2023-08-21 13:05:45 +0200

    Remove savePosition param from subsequent navigations (#447)

--- modules/vue-router/config/VUE_ROUTER_SCROLL_BEHAVIOR.ts
+++ modules/vue-router/config/VUE_ROUTER_SCROLL_BEHAVIOR.ts
@@ -1,9 +1,20 @@
 import { RouterScrollBehavior } from 'vue-router'
 
 const scrollBehavior: RouterScrollBehavior = (to, from, savedPosition) => {
-  if (savedPosition) return savedPosition
-  if (to.params.savePosition) return false
-  if (to.hash) return { el: to.hash, behavior: 'smooth' }
+  if (savedPosition) {
+    return savedPosition
+  }
+
+  if (to.params.savePosition) {
+    // For some reason vue-router maintains params between pushes.
+    // By removing savePosition from params, we prevent subsequent pushes from scrolling to the top of the page.
+    delete to.params.savePosition
+    return false
+  }
+
+  if (to.hash) {
+    return { el: to.hash, behavior: 'smooth' }
+  }
 
   return { top: 0, behavior: 'smooth' }
 }
