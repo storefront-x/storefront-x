15715867eb070b718ef6cfbfbe3b0bb8aa784cce
Adam Jedlička <mail@adamjedlicka.cz>
2023-09-05 09:44:47 +0200

    Improve typing and error handling of useResource (#449)

--- modules/vue/composables/useResource.ts
+++ modules/vue/composables/useResource.ts
@@ -1,10 +1,10 @@
 import useId from '#ioc/composables/useId'
 import useState from '#ioc/composables/useState'
 import IS_SERVER from '#ioc/config/IS_SERVER'
-import { shallowRef, watch } from 'vue'
+import { Ref, shallowRef, watch } from 'vue'
 
-async function useResource<T>(source: () => Promise<T>): Promise<[T]>
-async function useResource<T, U>(source: () => U, fetcher?: (source: U) => Promise<T>): Promise<[T]>
+async function useResource<T>(source: () => Promise<T>): Promise<[Ref<T>]>
+async function useResource<T, U>(source: () => U, fetcher?: (source: U) => Promise<T>): Promise<[Ref<T>]>
 async function useResource(source: any, fetcher?: any) {
   if (!fetcher) {
     fetcher = source
@@ -17,23 +17,27 @@ async function useResource(source: any, fetcher?: any) {
   const id = useId(sourceIdent)
   const state = useState<any>(id, () => undefined)
 
-  const promise = new Promise<[any]>((resolve) => {
+  const promise = new Promise<[any]>((resolve, reject) => {
     watch(
       [source],
       async ([source]) => {
-        if (state.value) {
-          response.value = state.value
+        try {
+          if (state.value) {
+            response.value = state.value
 
-          state.value = null
-        } else {
-          response.value = await fetcher(source)
+            state.value = null
+          } else {
+            response.value = await fetcher(source)
 
-          if (IS_SERVER) {
-            state.value = response.value
+            if (IS_SERVER) {
+              state.value = response.value
+            }
           }
-        }
 
-        resolve([response])
+          resolve([response])
+        } catch (error) {
+          reject(error)
+        }
       },
       {
         immediate: true,
