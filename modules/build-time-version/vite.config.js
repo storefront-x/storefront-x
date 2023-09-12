b6bada2e277c14e0b0b2aa8370be050b1bc8bd78
Adam Jedlička <mail@adamjedlicka.cz>
2023-09-12 22:33:28 +0200

    Refactoring

--- modules/build-time-version/vite.config.js
+++ modules/build-time-version/vite.config.js
@@ -1,5 +1,23 @@
 import { defineConfig } from 'vite'
-import version from './src/plugins/version'
+
+const version = () => {
+  const buildTime = new Date()
+  const virtualFileId = 'virtual:build-time-version'
+
+  return {
+    name: 'build-time-version-plugin',
+    resolveId(id) {
+      if (id === virtualFileId) {
+        return virtualFileId
+      }
+    },
+    load(id) {
+      if (id === virtualFileId) {
+        return `export default '${buildTime.toISOString()}'`
+      }
+    },
+  }
+}
 
 export default defineConfig({
   plugins: [version()],
