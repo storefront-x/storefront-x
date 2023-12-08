c59c2b2d86f73b21aa5e9023da0f7c8f9a373645
Adam Jedlička <mail@adamjedlicka.cz>
2023-12-08 11:34:16 +0100

    Fix global styles order

---
+++ modules/vue/tests/playwright/GlobalStyles.spec.js
@@ -0,0 +1,40 @@
+import { test, expect } from '@playwright/test'
+import { buildProject } from '@storefront-x/testing'
+
+test.only('global styles are rendered before components styles', async ({ page }) => {
+  await buildProject(
+    {
+      modules: [
+        '@storefront-x/base',
+        '@storefront-x/vue',
+        '@storefront-x/vue-router',
+        [
+          'my-module',
+          {
+            global: {
+              styles: {
+                'global.css': 'body { background-color: red }',
+              },
+            },
+            pages: {
+              'my-page.vue': `
+                <template>
+                <h1>MyComponent</h1>
+                </template>
+                <style scoped>
+                  h1 { color: blue }
+                </style>
+              `,
+            },
+          },
+        ],
+      ],
+    },
+    async ({ url }) => {
+      const response = await page.goto(url + '/my-page')
+      expect(await response.text()).toMatch(
+        /<style>body{background-color:red}<\/style>\s*<link rel="stylesheet" href="\/assets\/my-page-/,
+      )
+    },
+  )
+})
--- modules/vue/vue/plugins/renderGlobalStyles.server.ts
+++ modules/vue/vue/plugins/renderGlobalStyles.server.ts
@@ -4,7 +4,7 @@ import globalStyles from '~/.sfx/global/styles'
 export const after = async (app: App, ctx: any) => {
   ctx.out.globalStyles = (html: string) => {
     for (const style of Object.values(globalStyles)) {
-      html = html.replace('</head>', `<style>${style}</style>\n</head>`)
+      html = html.replace('<link rel="stylesheet"', `<style>${style}</style>\n<link rel="stylesheet"`)
     }
 
     return html
