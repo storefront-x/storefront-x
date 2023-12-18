1fc226ee41aa9ae8668dfc34c36d21d75b4d63d2
Adam Jedlička <mail@adamjedlicka.cz>
2023-12-18 11:42:35 +0100

    Fix tests

--- modules/vue/vue/plugins/renderGlobalStyles.server.ts
+++ modules/vue/vue/plugins/renderGlobalStyles.server.ts
@@ -1,10 +1,12 @@
 import type { App } from 'vue'
 import globalStyles from '~/.sfx/global/styles'
 
+const anchor = '<meta http-equiv="X-UA-Compatible" content="ie=edge" />'
+
 export const after = async (app: App, ctx: any) => {
   ctx.out.globalStyles = (html: string) => {
     for (const style of Object.values(globalStyles)) {
-      html = html.replace('<link rel="stylesheet"', `<style>${style}</style>\n<link rel="stylesheet"`)
+      html = html.replace(anchor, `${anchor}\n<style>${style}</style>`)
     }
 
     return html
