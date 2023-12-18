03834f59d2c293220c3686b912e132740e84d4c9
Adam Jedlička <mail@adamjedlicka.cz>
2023-12-18 15:45:43 +0100

    Fix global styles order (#459)

--- modules/vue/vue/plugins/renderGlobalStyles.server.ts
+++ modules/vue/vue/plugins/renderGlobalStyles.server.ts
@@ -1,10 +1,12 @@
 import type { App } from 'vue'
 import globalStyles from '~/.sfx/global/styles'
 
+const anchor = '<meta http-equiv="X-UA-Compatible" content="ie=edge" />'
+
 export const after = async (app: App, ctx: any) => {
   ctx.out.globalStyles = (html: string) => {
     for (const style of Object.values(globalStyles)) {
-      html = html.replace('</head>', `<style>${style}</style>\n</head>`)
+      html = html.replace(anchor, `${anchor}\n<style>${style}</style>`)
     }
 
     return html
