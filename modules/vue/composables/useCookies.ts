1a984185f8123d879bdc476af92697f1b105e455
Adam Jedlička <mail@adamjedlicka.cz>
2024-01-07 15:51:01 +0100

    Cookies are shared per request on the server side

--- modules/vue/composables/useCookies.ts
+++ modules/vue/composables/useCookies.ts
@@ -3,11 +3,14 @@ import { useCookies, createCookies } from '@vueuse/integrations/useCookies'
 import IS_CLIENT from '#ioc/config/IS_CLIENT'
 import useContext from '#ioc/composables/useContext'
 
-export default () => {
+export default (): ReturnType<typeof useCookies> => {
   if (IS_CLIENT) {
     return useCookies()
   } else {
     const ctx = useContext()
+
+    if (ctx.__cookies) return ctx.__cookies
+
     const cookies = createCookies(ctx.event.node.req)()
 
     cookies.addChangeListener((change) => {
@@ -17,6 +20,8 @@ export default () => {
         setCookie(ctx.event, change.name, change.value, change.options)
       }
     })
+
+    ctx.__cookies = cookies
 
     return cookies
   }
--- modules/vue/tests/playwright/useCookies.spec.js
+++ modules/vue/tests/playwright/useCookies.spec.js
@@ -45,3 +45,43 @@ test('setting of cookies on the server side', async ({ page }) => {
     },
   )
 })
+
+test('cookies are shared on the server side', async ({ page }) => {
+  await makeProject(
+    {
+      modules: [
+        '@storefront-x/base',
+        '@storefront-x/vue',
+        '@storefront-x/vue-router',
+        [
+          'my-module',
+          {
+            pages: {
+              'index.vue': `
+                <template>
+                  <h1>{{ cookies2.get('foo') }}</h1>
+                </template>
+                <script setup>
+                import IS_SERVER from '#ioc/config/IS_SERVER'
+                import useCookies from '#ioc/composables/useCookies'
+                import { ref, onMounted } from 'vue'
+
+                const cookies1 = useCookies()
+                const cookies2 = useCookies()
+
+                if (IS_SERVER) {
+                  cookies1.set('foo', 'bar')
+                }
+                </script>
+              `,
+            },
+          },
+        ],
+      ],
+    },
+    async ({ url }) => {
+      const response = await page.goto(url)
+      expect(await response.text()).toContain('<h1>bar</h1>')
+    },
+  )
+})
