3eec5d5e6784b2c73cb27d99e97c99e0522ff344
Martin Fajman <fajmanm@magexo.cz>
2024-01-19 14:54:01 +0100

    SFX-410: add pw test

---
+++ modules/vue-pinia-shared-state/tests/playwright/SharedState.spec.js
@@ -0,0 +1,56 @@
+import { test, expect } from '@playwright/test'
+import { makeProject } from '@storefront-x/testing'
+
+test('shared state test', async ({ page, context }) => {
+  await makeProject(
+    {
+      modules: [
+        '@storefront-x/base',
+        '@storefront-x/vue',
+        '@storefront-x/vue-router',
+        '@storefront-x/vue-pinia',
+        '@storefront-x/vue-pinia-shared-state',
+        [
+          'my-module',
+          {
+            pages: {
+              'index.vue': `
+                <template>
+                  <button @click="updateStoreCount">{{ mainStore.count }}</button>
+                </template>
+                <script setup>
+                import useMainStore from '#ioc/stores/useMainStore'
+
+                const mainStore = useMainStore()
+
+                const updateStoreCount = ()=> {
+                  mainStore.$patch({ count: 1 })
+                }
+                </script>
+              `,
+            },
+            stores: {
+              'useMainStore.ts': `
+                import defineStore from '#ioc/utils/vuePinia/defineStore'
+                export default defineStore('main', {
+                  state: () => ({
+                    count: 0,
+                  }),
+                })
+              `,
+            },
+          },
+        ],
+      ],
+    },
+    async ({ url }) => {
+      await page.goto(url, { waitUntil: 'networkidle' })
+      await page.locator('button').click()
+      await expect(page.locator('button')).toContainText('1')
+
+      const newTab = await context.newPage()
+      await newTab.goto(url, { waitUntil: 'networkidle' })
+      await expect(newTab.locator('button')).toContainText('1')
+    },
+  )
+})
