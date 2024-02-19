5338105bfe2d67f03ea0ee4d47bee76020d0c132
JanVanekRelitas <janvanek@relitas.cz>
2024-02-19 10:46:44 +0100

    demo magento revert

--- modules/create-storefront-x/template/magento/modules/demo-magento/tailwind.config.js
+++ modules/create-storefront-x/template/magento/modules/demo-magento/tailwind.config.js
@@ -1,3 +1,4 @@
+import colors from 'tailwindcss/colors.js'
 import forms from '@tailwindcss/forms'
 
 export default {
@@ -6,42 +7,11 @@ export default {
     './node_modules/@storefront-x/theme-tailwind-magento/**/*.vue',
     './modules/demo-magento/**/*.vue',
   ],
-  plugins: [
-    forms,
-    function ({ addComponents }) {
-      addComponents({
-        '.container': {
-          '@screen lg': {
-            maxWidth: '1280px',
-          },
-          '@screen xl': {
-            maxWidth: '1450px',
-          },
-          '@screen 2xl': {
-            maxWidth: '1920px',
-          },
-        },
-      })
-    },
-  ],
+  plugins: [forms],
   theme: {
     extend: {
       colors: {
-        primary: {
-          500: '#000000',
-          550: '#1a1b1e',
-          650: '#383838',
-          750: '#6b6b6b',
-        },
-        secondary: {
-          500: '#d33d01',
-        },
-        yellow: {
-          550: '#b89e60',
-        },
-        grey: {
-          550: '#f4f4f4',
-        },
+        primary: colors.orange,
       },
     },
   },
