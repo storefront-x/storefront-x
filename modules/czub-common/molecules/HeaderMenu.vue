ea087586a0be77dae33919a39aed2738a5eed83c
JanVanekRelitas <janvanek@relitas.cz>
2024-02-23 16:23:43 +0100

    remove magento

--- modules/czub-common/molecules/HeaderMenu.vue
+++ modules/czub-common/molecules/HeaderMenu.vue
@@ -7,6 +7,7 @@
       classes="p-2 text-primary-500 !font-bold text-base transition ease-in-out delay-[10ms] hover:bg-secondary-500"
       color="primary"
       @mouseenter="onMouseEnter(category.id)"
+      @mouseleave="onMouseLeave(category.id)"
     >
       <FlyoutMenu v-if="selectedCategoryId === category.id" :categories="category.children" />
     </CategoryLink>
