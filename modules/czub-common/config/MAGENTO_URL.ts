ca7e1e1b28b4b9159c9eab36cd3e0f2854d30ba5
JanVanekRelitas <janvanek@relitas.cz>
2024-03-29 15:57:18 +0100

    forgoten czub-common

--- modules/czub-common/config/MAGENTO_URL.ts
+++
@@ -1,3 +0,0 @@
-import getRuntimeConfigValue from '#ioc/utils/getRuntimeConfigValue'
-
-export default getRuntimeConfigValue('MAGENTO_URL') ?? 'https://czub-be-themes.magexo.cloud'
--- modules/czub-common/runtime/config/MAGENTO_URL.ts
+++
@@ -1,9 +0,0 @@
-export default getUrl(globalThis.process?.env?.MAGENTO_URL)
-
-function getUrl(url: string | undefined) {
-  if (!url) {
-    return undefined
-  }
-  const fixedUrl = url.startsWith('http') ? url : `https://${url}`
-  return fixedUrl
-}
